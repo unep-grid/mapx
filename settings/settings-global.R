9d16d79bf0cf937414be86c951124c5108a54167
fxi <f@fxi.io>
2017-03-27 16:47:04 +0200

    updated local settings and gitignore

--- settings/settings-global.R
+++ settings/settings-global.R
@@ -22,10 +22,41 @@ config[["ssh"]] <- list(
   UserKnownHostsFile="/dev/null",
   StrictHostKeyChecking="no",
   PasswordAuthentication="no",
-  IdentityFile="/Users/fxi/Documents/unep_grid/map-x/git/vagrant-map-x-full/.vagrant/machines/map-x-full/virtualbox/private_key",
+  IdentityFile="",
   IdentitiesOnly="yes",
   LogLevel="FATAL"
   )
+
+#
+# postgres configuration
+#
+config[["pg"]] = list(
+    host ='127.0.0.1',
+    dbname = 'mapx',
+    port = '5432',
+    user = 'mapxw',
+    encryptKey = "",
+    password= "",
+    conPool = 5,
+    geomCol = "geom",
+    tables = list(
+      "users"="mx_users",
+      "views"="mx_views",
+      "sources"="mx_sources"
+      )
+  )
+
+#
+# remote server configuration
+#
+config[["remote"]] <- list(
+  hostname="map-x-full",
+  host="127.0.0.1",
+  user="vagrant",
+  port=2222
+  )
+
+
 
 #
 # web resources : will be exposed to the client using shiny::addRessourcePath. 
@@ -213,34 +244,6 @@ config[["noData"]] <- list(
   "noFilter"
   )
 
-#
-# postgres configuration
-#
-config[["pg"]] = list(
-    host ='127.0.0.1',
-    dbname = 'mapx',
-    port = '5432',
-    user = 'mapxw',
-    encryptKey = readLines("../vagrant-map-x-full/passwords/generated/psql_crypto"),
-    password= readLines("../vagrant-map-x-full/passwords/generated/psql_mapx_write"),
-    conPool = 5,
-    geomCol = "geom",
-    tables = list(
-      "users"="mx_users",
-      "views"="mx_views",
-      "sources"="mx_sources"
-      )
-  )
-
-#
-# remote server configuration
-#
-config[["remote"]] <- list(
-  hostname="map-x-full",
-  host="127.0.0.1",
-  user="vagrant",
-  port=2222
-  )
 
 #
 # views configuration
