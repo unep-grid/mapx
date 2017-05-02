32193df24dd5d5fa3ce6280fcf3d27b4e98d09ad
fxi <f@fxi.io>
2017-05-02 15:55:45 +0200

    Try to render simplify in postgres before rendering geogjson

--- templates/pg/view_simple.sql
+++ templates/pg/view_simple.sql
@@ -1,4 +1,4 @@
-SELECT ST_AsGeoJSON({{geom|geom}},10) AS the_geom_geojson, {{variableName|}} 
+SELECT ST_AsGeoJSON(ST_SimplifyPreserveTopology({{geom|geom}},0.01),10) AS the_geom_geojson, {{variableName|}} 
 FROM {{layerName}}
 WHERE {{geom|geom}} && !bbox_4326!
 AND ST_Intersects( {{geom|geom}}, !bbox_4326! )
