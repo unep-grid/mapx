26b5c23aa958faee740135a1439e49eb20cb4e27
fxi <f@fxi.io>
2017-06-01 13:43:08 +0200

    formating; Updating tag filterting using the new function

--- templates/dot/view_popup.R
+++ templates/dot/view_popup.R
@@ -5,19 +5,27 @@ tags$div(
   tags$b( "{{=lay.title}}" ),
   tags$ul(class="list-group",
     "{{~lay.prop :prop}}",
-    "{{?prop.key.indexOf('mx_t') == -1}}", ## remove time propertie mx_t0 et mx_t1
+    #"{{?prop.key.indexOf('mx_t') == -1}}", ## remove time propertie mx_t0 et mx_t1
     tags$li(
       class='list-group-item', 
       style='max-width: 300px;',
       tags$b("{{=prop.key}}"),
       tags$span(
         HTML("{{ var tags = prop.value.toString().split(','); for(var t = 0; t < tags.length ; t++){ }}"),
-        tags$a(href="#",onclick="mgl.helper.filterViewValues({id:'{{=prop.idMap}}',idView:'{{=prop.idView}}',viewVariable:'{{=prop.key}}',search:'{{=tags[t]}}'})","{{=tags[t]}}"),
-        "{{ } }}",
-        tags$a(href="#",onclick="mgl.helper.resetViewStyle({id:'{{=prop.idMap}}',idView:'{{=prop.idView}}'})",icon("undo"))
+        tags$a(
+          href="#",
+          `data-tags_filter_id_map`="{{=prop.idMap}}",
+          `data-tags_filter_id_view`="{{=prop.idView}}",
+          `data-tags_filter_attribute`="{{=prop.key}}",
+          `data-tags_filter_search`="{{=tags[t]}}",
+          onClick="mgl.helper.filterViewValues(this)",
+          "{{=tags[t]}}"
+          ),
+         "{{ } }}",
+          tags$a(href="#",onclick="mgl.helper.resetViewStyle({id:'{{=prop.idMap}}',idView:'{{=prop.idView}}'})",icon("undo"))
         )
       ),
-    "{{?}}",
+    #"{{?}}",
     "{{~}}"
     ),
   "{{~}}"
