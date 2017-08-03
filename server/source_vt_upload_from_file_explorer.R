e04c3c2087374c1e848f561c0a9474fed4f0df69
fxi <f@fxi.io>
2017-08-03 14:03:00 +0200

    idProgress issue

--- server/source_vt_upload_from_file_explorer.R
+++ server/source_vt_upload_from_file_explorer.R
@@ -119,7 +119,7 @@ observeEvent(input$btnImportNewSource,{
       #
       # Start progress
       #
-      mxProgress(id=idProgress, text= "Start importation, please wait", percent=1)
+      mxProgress(id=idSource, text= "Start importation, please wait", percent=1)
       #
       # Create new row
       #
