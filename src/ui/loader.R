ee1aefb15b18ccd06d81bdba1900d210797b770d
fxi <f@fxi.io>
2017-12-01 12:46:08 +0100

    Added better loader

---
+++ src/ui/loader.R
@@ -0,0 +1,18 @@
+
+#https://loading.io/css/
+tags$div(
+  class="mx-loader-container",
+  tags$div(
+    class="mx-loader-spinner-container",
+    tags$div(
+      class="mx-loader-spinner",
+      style="width:100%;height:100%",
+      tags$div(),
+      tags$div(),
+      tags$div(),
+      tags$div()
+      )
+    )
+  )
+
+
