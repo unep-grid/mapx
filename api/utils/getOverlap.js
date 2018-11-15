88b00033b4c29ea9af178987fd1632d19d117bde
Thomas Piller <thomas.piller@unepgrid.ch>
2018-11-15 16:16:34 +0100

    Fixes bug with non-overlapping layers

--- api/utils/getOverlap.js
+++ api/utils/getOverlap.js
@@ -1,68 +1,75 @@
-const { Pool, Client } = require('pg');
-const parseWKT  = require('wellknown');
-const martinez  = require('martinez-polygon-clipping');
-const turf  = require('@turf/turf');
+const parseWKT = require('wellknown');
+const martinez = require('martinez-polygon-clipping');
+const turf = require('@turf/turf');
 const clientPgRead = require.main.require("./db").pgWrite;
-const zlib  = require("zlib");
 
 
-exports.get = function(req,res,next){
-
+exports.get = function (req, res, next) {
 
   var start = Date.now();
-  var layers = req.query.layers ? req.query.layers.split(',') || [] : [] ;
-  var countries = req.query.countries ? req.query.countries.split(',') || [] : [] ;
-  var method = req.query.method || 'getArea'; 
+  var layers = req.query.layers ? req.query.layers.split(',') || [] : [];
+  var countries = req.query.countries ? req.query.countries.split(',') || [] : [];
+  var method = req.query.method || 'getArea';
   res.setHeader('Content-Type', 'application/json');
 
-  if( method == 'getArea' ){
-    return getOverlapArea( countries, layers, {
-      message : function(msg){
+  if (method == 'getArea') {
+    return getOverlapArea(countries, layers, {
+      message: function (msg) {
         res.write(toString({
-          type:'message',
+          type: 'message',
           msg: msg
         }));
+        res.write(toString({
+          type: 'timing',
+          msg: Date.now() - start,
+          unit: 'ms'
+        }));
       },
-      result : function(msg){
+      result: function (msg) {
 
         res.write(toString({
-          type:'timing',
+          type: 'timing',
           msg: Date.now() - start,
-          unit : 'ms'
+          unit: 'ms'
         }));
 
         res.write(toString({
-          type:'result',
+          type: 'result',
           msg: msg,
-          unit : 'm2'
+          unit: 'm2'
         }));
         res.end();
       }
     })
       .catch(e => {
         res.write(toString({
-          type:'error',
-          msg:  e.message
+          type: 'error',
+          msg: e.message
         }));
         res.end();
       });
-  }else{
+  } else {
     res.write(toString({
-      type:'error',
+      type: 'error',
       msg: 'Method ' + method + ' not yet handled'
     }));
     res.end();
   }
 };
 
-function getOverlapArea(countries,layers,send){
+function getOverlapArea(countries, layers, send) {
 
-  return new Promise(( resolve, reject ) => {
+  var queryCountries = {};
+  var dataCountries = [];
+  var dataLayers = [];
+  var area = 0;
+
+  return new Promise((resolve, reject) => {
 
     send.message = send.message || console.log;
     send.result = send.result || console.log;
 
-    send.message('Start overlap with countries = ' + JSON.stringify(countries) + ' and layers = ' + JSON.stringify(layers) );
+    send.message('Start overlap with countries = ' + JSON.stringify(countries) + ' and layers = ' + JSON.stringify(layers));
 
     var hasCountries = countries.length > 0;
     var queryLayers = [];
@@ -74,12 +81,12 @@ function getOverlapArea(countries,layers,send){
     }
 
     // Test layers input
-    if (layers.length === 0 && layers.length <= 3) {
+    if (layers.length === 0 || layers.length > 3) {
       throw new Error("The number of layers is invalid!");
     }
 
     // Parameterized query that returns a GeometryCollection containing the countries' geom
-    const queryCountries = {
+    queryCountries = {
       text: `SELECT ST_AsText(ST_Buffer(ST_Collect(geom),0)) as geom
       FROM mx_countries 
       WHERE CASE WHEN $1 
@@ -103,7 +110,7 @@ function getOverlapArea(countries,layers,send){
           SELECT ST_AsText(ST_Buffer(ST_Collect(l.geom),0)) 
           FROM ${layers[i]} l, countries c
           WHERE l.geom && c.geom AND ST_Intersects(l.geom,c.geom)`,
-          values: [ countries ],
+          values: [countries],
           rowMode: 'array'
         };
       } else if (hasCountries === false) {
@@ -117,65 +124,88 @@ function getOverlapArea(countries,layers,send){
     }
 
     // Merge all SQL queries in one array
-    queryLayers.push(queryCountries);
-    
+    //queryLayers.push(queryCountries);
+
 
     // Query to fetch the countries & layers geometries + conversion to GeoJSON
-    var promLayers = queryLayers.map(( l,i )=> {
+    var promLayers = queryLayers.map((l, i) => {
       return clientPgRead.query(l)
-        .then( r => {
+        .then(res => {
           send.message('Extract data of layer' + i);
-          var a = r.rows[0];
-          a = a instanceof Array ? a[0] : null;
-          return a ? parseWKT(a) : a;
+          return parseWKTArr(res);
         });
     });
- 
+
     resolve(promLayers);
 
   })
-    .then( promLayers => {
+    .then(promLayers => {
 
       send.message('Extract all data');
       return Promise.all(promLayers);
     })
-    .then(res => {
+    .then(dataLayersJSON => {
 
-      send.message('Data extracted : ' + res.length + ' layers');
-      var area = null;
-      var resFiltered = res.filter(function (el) {
+      send.message('Data extracted : ' + dataLayersJSON.length + ' layers');
+
+      var dataLayersJSONFiltered = dataLayersJSON.filter(function (el) {
         return el != null;
       });
 
-      if (resFiltered.length < 2) {
-        throw new Error("Intersection cannot be compute because the number of layer is smaller than 2!");
+      if (dataLayersJSON.length >= 2 && dataLayersJSON.length === dataLayersJSONFiltered.length) {
+        dataLayers = dataLayersJSONFiltered.reduce((intersection, layer, index) => {
+          var lc = layer.coordinates;
+          intersection = intersection instanceof Array ? intersection : intersection.coordinates;
+          send.message('Intersect between layer ' + index + ' and previous');
+          if (areCoordsValid(lc, intersection)) {
+            return martinez.intersection(lc, intersection);
+          } else {
+            return [];
+          }
+        });
+      } else if (dataLayersJSON.length === 1 && dataLayersJSONFiltered.length === 1) {
+        dataLayers = dataLayersJSONFiltered[0].coordinates;
+      } else {
+        dataLayers = [];
       }
 
-      let m = resFiltered.reduce((intersection, layer, index) => {
-        var lc = layer.coordinates;
-        intersection = intersection instanceof Array ? intersection : intersection.coordinates;
-        send.message('Intersect between layer ' + index + ' and previous');
-        if (areCoordsValid(lc, intersection)) {
-          return martinez.intersection(lc, intersection);
-        } else {
-          throw new Error('Invalid coordinates for layer ' + index );
+      if (dataLayers instanceof Array && dataLayers.length > 0) {
+        return clientPgRead.query(queryCountries);
+      } else {
+        return Promise.resolve(false);
+      }
+    })
+    .then(dataCountriesWKT => {
+
+      if (!dataCountriesWKT) {
+        send.message('No layers intersect, skip countries');
+      } else {
+        send.message('Extract data of countries');
+        dataCountries = parseWKTArr(dataCountriesWKT);
+        var cCountries = dataCountries.coordinates;
+        var cLayers = dataLayers;
+        var cIntersect = [];
+        var cIntersectMultiPolygon = {};
+
+        send.message('Intersect between the layers intersection and countries');
+        if (areCoordsValid(cCountries, cLayers)) {
+          cIntersect = martinez.intersection(cCountries, cLayers);
         }
-      });
 
-
-      if (m instanceof Array) {
-        send.message('Build geometry');
-        m = turf.multiPolygon(m);
-        send.message('Compute area');
-        area = turf.area(m);
+        if (cIntersect instanceof Array && cIntersect.length > 0) {
+          send.message('Build geometry');
+          cIntersectMultiPolygon = turf.multiPolygon(cIntersect);
+          send.message('Compute area');
+          area = turf.area(cIntersectMultiPolygon);
+        }
       }
 
       send.message('Send result');
       send.result(area);
       return area;
     });
-
 }
+
 /** helpers 
  * 
  * 
@@ -191,13 +221,20 @@ function areCoordsValid(a, b) {
   return a instanceof Array && b instanceof Array && a.length > 0 && b.length > 0;
 }
 
+/* Parse & stringify Well-Known Text (WKT) into GeoJSON
+ *  @param {Array} res Array of WKT geometries (as string)
+ *  @return {Boolean} GeoJSON geometry object or null if parse fails
+ */
+function parseWKTArr(res) {
+  var a = res.rows[0];
+  a = a instanceof Array ? a[0] : null;
+  return a ? parseWKT(a) : a;
+}
+
 /**
  * Send string for message
  * @param {Object} obj object to be converted in string for messages
  */
-function toString(obj){
-  return JSON.stringify(obj)+"\t\n";
+function toString(obj) {
+  return JSON.stringify(obj) + "\t\n";
 }
-
-
-
