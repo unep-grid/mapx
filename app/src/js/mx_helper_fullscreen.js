0bd49d9f9fb61ee69174497e4f6a95c93808c12a
fxi <f@fxi.io>
2018-11-20 15:13:15 +0100

    Use require instead of import for screenfull : browsers does not like if the fullscreen mode is not imediately requested after the user click. So, not possible to load module asynchronously

--- app/src/js/mx_helper_fullscreen.js
+++ app/src/js/mx_helper_fullscreen.js
@@ -1,30 +1,32 @@
 /*jshint esversion: 6 , node: true */
 
+/*
+* Async load does not work : browser think the user did not press the button hersef
+*/
+var screenfull = require('screenfull');
+
 export function toggleFullScreen(id) {
 
-  return import("screenfull")
-    .then( screenfull => {
-      var el = document.body;
-      var btn;
+  var el = document.body;
+  var btn;
 
-      btn = document.getElementById(id).querySelector(".fa");
+  btn = document.getElementById(id).querySelector(".fa");
 
-      var isEnabled = btn.classList.contains('fa-compress');
+  var isEnabled = btn.classList.contains('fa-compress');
 
-      if(screenfull.enabled){
-        if(!isEnabled){
-          btn.classList.add("fa-compress");
-          btn.classList.remove("fa-expand");
-          screenfull.request();
-        }else{
-          btn.classList.remove("fa-compress");
-          btn.classList.add("fa-expand");
-          screenfull.exit();
-        }
-      }else{
-        alert("Fullscreen not enabled, sorry");
-      }
-    });
+  if(screenfull.enabled){
+    if(!isEnabled){
+      btn.classList.add("fa-compress");
+      btn.classList.remove("fa-expand");
+      screenfull.request();
+    }else{
+      btn.classList.remove("fa-compress");
+      btn.classList.add("fa-expand");
+      screenfull.exit();
+    }
+  }else{
+    alert("Fullscreen not enabled, sorry");
+  }
 }
 
 
