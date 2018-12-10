0ea4c178d1975d5a3d449d96ed06fa60cbcdd414
fxi <f@fxi.io>
2018-12-10 12:36:40 +0100

    Solve issue with curl that does not accept anymore host without protocol in url

---
+++ app/src/helper/email_sender.R
@@ -0,0 +1,72 @@
+#' Send an email using local or remote 'mail' command
+#' @param subject. String. Test for the subject 
+#' @param from String. Valid email for  sender
+#' @param to String. Valid email for Recipient
+#' @param body String. Text of the body
+#' @param type String. "text" or "html"
+#' @param wait Boolean. Wait for the mail to be sent
+#' @export
+mxSendMail <- function( from=NULL, to=NULL, type="text", body=NULL,bodyHTML=NULL, subject=NULL, wait=F ){
+
+
+  if(noDataCheck(from)){
+    from <- .get(config,c("mail","bot"))
+  }
+
+  if(noDataCheck(subject)){
+    subject = "map-x"
+  }
+
+  if(noDataCheck(body)){
+    stop("empty message")
+  }else{
+    body <- as.character(body)
+  }
+
+  if(noDataCheck(bodyHTML)){
+    bodyHTML <- ""
+  }else{
+    bodyHTML <- as.character(bodyHTML)
+  }
+
+
+  if( ! mxEmailIsValid(from) | ! mxEmailIsValid(to)) stop(paste("mxSendMail : email not valid. From: ", from, " To: ", to))
+
+  msgClear <- list(
+      from = from,
+      to = to,
+      subject = subject,
+      text = body,
+      html = bodyHTML,
+      validUntil = as.character(Sys.Date() + 1)
+      )
+
+  msg <- mxDbEncrypt(msgClear)
+
+  data = toJSON(list(msg = msg ),auto_unbox=T)
+
+  if( F && Sys.getenv("API_PORT") != "80" ){
+    mxDebugMsg(msgClear)
+    return("ok")
+  }
+
+  host <-  .get(config,c("api","host")) 
+  url <- "http://api:3030/send/mail"
+  h <- new_handle(copypostfields = data)
+  handle_setheaders(h,
+    "Content-Type" = "application/json",
+    "Cache-Control" = "no-cache",
+    "Host" = host 
+    )
+  
+  req <- curl_fetch_memory(url, handle = h)
+  
+  res <- fromJSON(rawToChar(req$content))
+
+  if(to %in% res$accepted){
+    return("ok")
+  }else{
+    stop("mxSendMail : Server can't send email to " + to )
+  }
+
+}
