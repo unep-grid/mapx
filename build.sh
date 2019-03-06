02aeda6679fdf94969bfdeeccdf692d9d70ba6f7
fxi <f@fxi.io>
2019-03-06 17:12:10 +0100

    Upate project building script, linting configuration, changelog + minor changes

--- build.sh
+++ build.sh
@@ -13,6 +13,10 @@ FG_NORMAL="\033[0m"
 CHANGES_CHECK=$(git status --porcelain | wc -l)
 CUR_HASH=$(git rev-parse HEAD)
 USAGE="Usage : bash build.sh $OLD_VERSION"
+PREVIOUS_LOG=$(git log -1 --pretty=%B)
+CHANGELOG_FILE="CHANGELOG.md"
+BASE_URL_CHANGELOG="https://github.com/fxi/map-x-mgl/tree/"
+
 
 if [ $CHANGES_CHECK -gt 0 ]
 then 
@@ -46,7 +50,7 @@ perl -pi -e $REP_APP_TAG ./docker-compose.yml
 
 git --no-pager diff --minimal 
 
-echo "Verify git diff of versioning changes. If its ok, type YES, else NO"
+echo "Verify git diff of versioning changes. Continue ? [YES/NO]"
 read confirm_diff
 
 if [ "$confirm_diff" != "YES"  ]
@@ -97,6 +101,31 @@ then
   git commit -m "auto build version $NEW_VERSION"
   git tag $NEW_VERSION
   git push $REMOTE $BRANCH --tags
+fi
+
+echo "Paste latest hash and message to the changelog ? [YES/NO]"
+read confirm_changelog_edit
+
+if [ "$confirm_changelog_edit" = "YES"  ]
+then 
+  LATEST_HASH=$(git log -1 --pretty=%T)
+  UPDATE_URL='-<a href="'$BASE_URL_CHANGELOG$LATEST_HASH'">'$NEW_VERSION'</a>'
+  UPDATE_TXT=$UPDATE_URL"\n"$PREVIOUS_LOG"\n"
+  # edit changelog
+  perl -pi -e 'print "'$UPDATE_TXT'" if $. == 2' $CHANGELOG_FILE
+  vim CHANGELOG_FILE
+  # Check changes
+  git --no-pager diff --minimal 
+  echo "Confirm changelog commit ? [YES/NO]"
+  read confirm_changelog_commit
+
+  if [ "$confirm_changelog_commit" = "YES"  ]
+    git add .
+    git commit -m "auto update changelog"
+    git push $REMOTE $BRANCH
+  then 
+    echo "Uncommited changes"
+  fi
 fi
 
 echo "Done"
