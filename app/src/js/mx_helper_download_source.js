09f59998af1cf7b22c2bef12022b4a1cea2830d7
fxi <f@fxi.io>
2019-04-09 14:23:19 +0200

    Enable translation in source dowload. Api messages will be translated later.

--- app/src/js/mx_helper_download_source.js
+++ app/src/js/mx_helper_download_source.js
@@ -1,116 +1,150 @@
-
-export function handlerDownloadVectorSource(o){
-  var elMsg;
+export function handlerDownloadVectorSource(o) {
   var elOutput = document.getElementById(o.idHandlerContainer);
 
   var dlUrlCreate = mx.helpers.getApiUrl('downloadSourceCreate');
   var dlUrlGet = mx.helpers.getApiUrl('downloadSourceGet');
 
-  dlUrlCreate = dlUrlCreate +'?'+ mx.helpers.objToParams(o.request);
+  dlUrlCreate = dlUrlCreate + '?' + mx.helpers.objToParams(o.request);
 
-  if(!elOutput){
-    throw new Error("No handler container");
+  if (!elOutput) {
+    throw new Error('No handler container');
   }
+  var h = mx.helpers;
+  var el = h.el;
 
-  var el = mx.helpers.el;
-
-
-  /* progress bar */
-  var progressMsg = "";
-  var elProgressLabel =  el("label","Progress");
-
-  var elProgressBar = el("div",{
-    class: 'mx-inline-progress-bar'
-  });
-  var elProgressBarContainer = el("div",{
-    class: 'mx-inline-progress-container'
-  },
-    elProgressBar
-  );
   /* log messages */
-  var elLogLabel =  el("label","Logs");
-  var elProgressMessageContainer = el("div",{
-    class: ['form-control', 'mx-logs']
-  });
+  var elLogLabel, elProgressBar, elProgressBarContainer, elProgressLabel, elProgressMessage, elProgressMessageContainer;
 
-  var elProgressMessage = el("ul");
-
-  elProgressMessageContainer.appendChild(elProgressMessage);
-
-  var elProgressContainer = el("div",
-    elProgressLabel,
-    elProgressBarContainer,
-    elLogLabel,
-    elProgressMessageContainer
+  var elProgressContainer = el(
+    'div',
+    /**
+     * Progress bar
+     */
+    (elProgressLabel = el('label', {dataset: {lang_key: 'api_progress_title'}})),
+    (elProgressBarContainer = el(
+      'div',
+      {
+        class: 'mx-inline-progress-container'
+      },
+      (elProgressBar = el('div', {
+        class: 'mx-inline-progress-bar'
+      }))
+    )),
+    /**
+     * Message box
+     */
+    (elLogLabel = el('label', {dataset: {lang_key: 'api_log_title'}})),
+    (elProgressMessageContainer = el(
+      'div',
+      {
+        class: ['form-control', 'mx-logs']
+      },
+      (elProgressMessage = el('ul'))
+    ))
   );
 
   elOutput.appendChild(elProgressContainer);
+  updateTranslation();
 
   mx.helpers.getJSON({
-    maxWait : 1e3 * 120,
-    url : dlUrlCreate,
-    onProgress : function(progress){
+    maxWait: 1e3 * 120,
+    url: dlUrlCreate,
+    onProgress: function(progress) {
       cleanMsg(progress);
+      updateTranslation();
     },
-    onMessage : function(data){
+    onMessage: function(data) {
       cleanMsg(data);
+      updateTranslation();
     },
-    onSuccess : function(data){
+    onSuccess: function(data) {
       cleanMsg(data);
+      updateTranslation();
     },
-    onError: function(er){
+    onError: function(er) {
       cleanMsg(er);
+      updateTranslation();
     }
   });
 
   var messageStore = {};
 
-  function cleanMsg(msg){
-    return mx.helpers.handleRequestMessage( msg, messageStore ,{
-      end : function(msg){
-        var li = el("li",{
-          class : ['mx-log-item','mx-log-green'] 
-        },el('p','Process done, the data is available for',
-          el('a','download',{
-            href  : dlUrlGet + msg.filepath,
-            target : '_blank'
-          })
-        ));
+  function updateTranslation() {
+    h.updateLanguageElements({el: elProgressContainer});
+  }
+
+  function cleanMsg(msg) {
+    return mx.helpers.handleRequestMessage(msg, messageStore, {
+      end: function(msg) {
+        var li = el(
+          'li',
+          {
+            class: ['mx-log-item', 'mx-log-green']
+          },
+          el('div',
+            el(
+              'p',
+              {
+                dataset: {
+                  lang_key: 'api_download_ready'
+                }
+              }),
+            el('a', {
+              dataset: {
+                lang_key: 'api_download_btn'
+              },
+              href: dlUrlGet + msg.filepath,
+              target: '_blank'
+            })
+          )
+        );
         elProgressMessage.appendChild(li);
       },
-      error :function(err){
-        var li = el("li",{
-          class:['mx-log-item','mx-log-red']},
+      error: function(err) {
+        var li = el(
+          'li',
+          {
+            class: ['mx-log-item', 'mx-log-red']
+          },
           err
         );
         elProgressMessage.appendChild(li);
       },
-      warning :function(err){
-        var li = el("li",{
-          class:['mx-log-item','mx-log-orange']},
+      warning: function(err) {
+        var li = el(
+          'li',
+          {
+            class: ['mx-log-item', 'mx-log-orange']
+          },
           err
         );
         elProgressMessage.appendChild(li);
       },
-      message : function(msg){
-        var li = el("li",{
-          class:['mx-log-item','mx-log-blue']},
+      message: function(msg) {
+        var li = el(
+          'li',
+          {
+            class: ['mx-log-item', 'mx-log-blue']
+          },
           msg
         );
         elProgressMessage.appendChild(li);
       },
-      progress : function(progress){
-        elProgressBar.style.width = progress + "%";
+      progress: function(progress) {
+        elProgressBar.style.width = progress + '%';
       },
-      default : function(msg){
-        if(msg && msg.length>3){
-          var li = el("li",{
-            class:['mx-log-item','mx-log-gray']},
-            msg);
+      default: function(msg) {
+        if (msg && msg.length > 3) {
+          var li = el(
+            'li',
+            {
+              class: ['mx-log-item', 'mx-log-gray']
+            },
+            msg
+          );
           elProgressMessage.appendChild(li);
         }
       }
     });
   }
 }
-
