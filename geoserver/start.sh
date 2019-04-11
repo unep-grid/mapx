490ee2f15b30780e0bedfd3e870096566f392d60
fxi <f@fxi.io>
2019-04-11 12:32:33 +0200

    Solve non updated password at start

--- geoserver/start.sh
+++ geoserver/start.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
+#
+# Set default credential using custom variable
+#
 updateCred="<?xml version='1.0' encoding='UTF-8'?>\
-<userRegistry version='1.0' xmlns='http://www.geoserver.org/security/users'>\
-<users><user enabled='true' name='admin' password='plain:"${GEOSERVER_ADMIN_PASSWORD}"'/></users>\
-<groups/></userRegistry>" 
+  <userRegistry version='1.0' xmlns='http://www.geoserver.org/security/users'>\
+  <users><user enabled='true' name='admin' password='plain:"${GEOSERVER_ADMIN_PASSWORD}"'/></users>\
+  <groups/></userRegistry>" 
 
-echo ${GEOSERVER_DATA_DIR} 
-echo ${GEOSERVER_HOME} 
+
+#
+# If the data dir does not exist, import the default one, else, do nothing
+#
+if [ ! -d ${GEOSERVER_DATA_DIR}/security ]
+then
+  cp -R ${GEOSERVER_HOME}/data_dir ${GEOSERVER_DATA_DIR}
+else
+  echo "Data folder ${GEOSERVER_DATA_DIR} exists, don't copy default"
+fi
+
+#
+# Update password, start the app
+#
+echo "Update default password"
 
 echo ${updateCred} > ${GEOSERVER_DATA_DIR}/security/usergroup/default/users.xml
-
-cat ${GEOSERVER_HOME}"/bin/startup.sh"
 
 sh ${GEOSERVER_HOME}"/bin/startup.sh"
 
