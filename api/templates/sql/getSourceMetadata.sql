1d8166bf478bec09352da85bec5bb91c3ffb93f0
fxi <f@fxi.io>
2019-09-03 11:52:21 +0200

    Adding 'soft' data download prevention in attribute table module. See #461 for details; minor changes

--- api/templates/sql/getSourceMetadata.sql
+++ api/templates/sql/getSourceMetadata.sql
@@ -1,6 +1,7 @@
 SELECT 
 s.data #> '{"meta"}' AS metadata, 
 s.date_modified AS date_modified, 
-u.email AS email_editor
+u.email AS email_editor,
+s.services AS services
 FROM mx_sources s, mx_users u
 WHERE s.id = '{{idSource}}' AND s.editor = u.id
