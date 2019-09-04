195f49bf05129975abf71c5cb2cfd08e6fb235d9
fxi <f@fxi.io>
2019-09-04 17:47:49 +0200

    Use single client in API, delegate pooling by pg_pool in docker

--- api/db/index.js
+++ api/db/index.js
@@ -1,33 +1,39 @@
-const s = require("./../settings");
-const { Pool, types } = require("pg");
-const redis = require("redis");
+const s = require('./../settings');
+const {Client, types} = require('pg');
+const redis = require('redis');
 
 /*
-* custom type parsing
-*/
-types.setTypeParser(1700, // numeric
+ * custom type parsing
+ */
+types.setTypeParser(
+  1700, // numeric
   function(val) {
-  return parseFloat(val);
+    return parseFloat(val);
+  }
+);
+
+var pgWrite = new Client({
+  host: s.db.host,
+  user: s.db.write.user,
+  database: s.db.name,
+  password: s.db.write.password,
+  port: s.db.port
 });
+pgWrite.connect();
+exports.pgWrite = pgWrite;
 
-
-exports.pgWrite = new Pool({
-  host : s.db.host,
-  user : s.db.write.user,
-  database : s.db.name,
-  password : s.db.write.password,
-  port : s.db.port 
-});
-
-exports.pgRead = new Pool({
-  host : s.db.host,
-  user : s.db.read.user,
-  database : s.db.name,
-  password : s.db.read.password,
-  port : s.db.port,
+var pgRead = new Client({
+  host: s.db.host,
+  user: s.db.read.user,
+  database: s.db.name,
+  password: s.db.read.password,
+  port: s.db.port,
   statement_timeout: 30 * 1000
 });
 
+pgRead.connect();
+exports.pgRead = pgRead;
+
 exports.redis = redis.createClient({
-  url: "redis://"+ s.redis.host + ":" + s.redis.port
+  url: 'redis://' + s.redis.host + ':' + s.redis.port
 });
