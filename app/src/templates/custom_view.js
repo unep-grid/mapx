3b423cb4d0cdd85e66d2b9dfb1fde904873f0fca
fxi <f@fxi.io>
2019-10-18 08:08:38 +0200

    Update custom view template

--- app/src/templates/custom_view.js
+++ app/src/templates/custom_view.js
@@ -1,28 +1,24 @@
-
 /**
-* Custom method for custom view in MapX
-* Parameters for onInit and onClose function ;
-* @param {Object} o Options
-* @param {Object} o.view Map-x view object
-* @param {Object} o.map mapbox-gl map object
-* @param {String} o.idView If of the view
-* @param {String} o.idSource Id of the source
-* @param {Element} o.elLegend Element containing the legend
-*
-*/
-
+ * Custom method for custom view in MapX
+ * Parameters for onInit and onClose function ;
+ * @param {Object} o Options
+ * @param {Object} o.view Map-x view object
+ * @param {Object} o.map mapbox-gl map object
+ * @param {String} o.idView If of the view
+ * @param {String} o.idSource Id of the source
+ * @param {Element} o.elLegend Element containing the legend
+ *
+ */
 return {
-  onClose : function(o){ 
+  onClose: function(o) {
     console.log("Custom view closed");
     /**
      * Remove source
      */
-    if( o.map.getSource( o.idSource ) ){
-      o.map.removeSource( o.idSource );
-    }
+    o.onRemove();
 
   },
-  onInit : function(o){ 
+  onInit: function(o) {
     console.log("Custom view added");
     /**
      * Set custom source
@@ -33,7 +29,7 @@ return {
         "type": "Feature",
         "geometry": {
           "type": "Point",
-          "coordinates": [ 6.111354, 46.213232 ]
+          "coordinates": [6.111354, 46.213232]
         },
         "properties": {
           "title": "Map-x Home"
@@ -58,12 +54,29 @@ return {
     /**
      * Set custom legend
      */
-    o.elLegend.innerText = "hello mapx";  
+    o.elLegend.innerText = "hello mapx";
 
     /**
-    * Set layer and source
-    */
-    o.map.addSource(o.idSource,o.source);
-    o.map.addLayer(o.layer,mx.settings.layerBefore);
+     * Set layer and source
+     */
+    o.map.addSource(o.idSource, o.source);
+    o.map.addLayer(o.layer, mx.settings.layerBefore);
+
+    /**
+     * Remove added layers
+     */
+    o.onRemove = function() {
+      var layer = o.map.getLayer(o.idView);
+      var source = o.map.getSource(o.idSource);
+      if (layer) {
+        o.map.removeLayer(o.idView);
+      }
+      if (source) {
+        o.map.removeSource(o.idSource);
+      }
+    };
+
   }
 };
+
+
