fb81eec25c9be51805fa637ebd0651d3f7598ade
Thomas Piller <thomas.piller@unepgrid.ch>
2019-11-11 16:46:01 +0100

    Merge routes for encrypted and non-encrypted queries, refactoring to improve error handling

--- api/utils/query.js
+++ api/utils/query.js
@@ -1,80 +1,109 @@
-const pgRead = require.main.require("./db").pgRead;
 const pgCustom = require.main.require("./db").pgCustom;
 const decrypt = require("./db.js").decrypt;
 const utils = require('./utils.js');
 const { Parser } = require('node-sql-parser');
 
+/**
+ * Set middleware chain for encrypted query and non-encrypted query
+ */
+exports.get = [decryptQuery, validateSql, querySql];
 
 /**
-* Get a custom encrypted request
-* data : {"type":"query","validUntil":{},"data":{"sql":"select count(*) from mx_uga_vector_9sfi1_5ol25_dtgas"}}
+* Decrypts query if needed
 */
-exports.get = function (req, res) {
-  var encryptedQuery = req.query.data;
-  decrypt(encryptedQuery)
-    .then(result => {
-      var data = result.data;
-      if (!data || !data.sql) throw new Error('Empty query');
-      return pgRead.query(data.sql);
-    })
-    .then(function (sqlRes) {
-      res.send(sqlRes.rows);
-    }).catch(function (err) {
-      res.status(403).send("Bad request " + err);
+function decryptQuery(req, res, next) {
+  var promDecrypt;
+  if (req.query.data) {
+    promDecrypt = decrypt(req.query.data)
+      .then(r => {
+        req.query.sql = r.data.sql;
+        next();
+      });
+  } else {
+    promDecrypt = new Promise(() => {
+      next();
     });
-};
+  }
+  return promDecrypt
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).send('500 Bad request');
+    });
+}
 
 /**
-* Get a custom non-encrypted request
+* Validates SQL query
 * NOTE: Non-SELECT queries are blocked by PostgreSQL
 */
-exports.getCustom = function (req, res) {
+function validateSql(req, res, next) {
   const parser = new Parser();
   const opt = {
-    database: 'PostgresQL'
+    database: 'PostgreSQL'
+  };
+
+  const messages = [];
+  var ast, sqlParsed;
+
+  const sql = req.query.sql;
+  if (!sql) {
+    messages.push('SQL argument must be provided.');
   }
-  return new Promise((resolve) => {
-    const sql = req.query.sql;
-    if (!sql) {
-      throw new Error('SQL argument must be provided.');
-    }
 
-    const ast = parser.astify(sql, opt);
+  if (messages.length === 0) {
+    try {
+      ast = parser.astify(sql, opt);
+    } catch (e) {
+      console.error(e.message);
+      messages.push('The SQL query could not be parsed.');
+    }
+  }
+
+  if (messages.length === 0) {
     if (Array.isArray(ast) === true) {
-      throw new Error('only one query can be executed at a time.');
+      messages.push('Only one query can be executed at a time.');
     }
 
-    var hasGeom = utils.findValues(ast, 'column').indexOf('geom') > -1
+    var hasGeom = utils.findValues(ast, 'column').indexOf('geom') > -1;
     if (hasGeom === true) {
-      throw new Error('The geom column cannot be exported or used by functions.');
+      messages.push('The geom column cannot be exported or used by functions.');
     }
 
     var hasStar = utils.findValues(ast, 'columns').indexOf('*') > -1
+      || utils.findValues(ast, 'column').indexOf('*') > -1
       || utils.findValues(ast, 'value').indexOf('*') > -1;
     if (hasStar === true) {
-      throw new Error('Asterisk cannot be used to select all columns or by functions.');
+      messages.push('Asterisk cannot be used to select all columns or by functions.');
     }
 
-    const sqlParsed = parser.sqlify(ast, opt);
+    try {
+      sqlParsed = parser.sqlify(ast, opt);
+    } catch (e) {
+      console.error(e.message);
+      messages.push('The AST object could not be parsed.');
+    }
+  }
 
-    resolve({
-      text: sqlParsed
+  if (messages.length === 0) {
+    req.sqlQuery = sqlParsed;
+    next();
+  } else {
+    res.send({
+      type: 'error',
+      msg: messages
     });
+  }
+}
 
-  })
-    .then((query) => {
-      /**
-      * Execute request
-      */
-      return pgCustom.query(query);
-    }).then((r) => {
-      /**
-      * Send raw result
-      */
+/**
+* Executes query and sends raw result
+*/
+function querySql(req, res) {
+  pgCustom.query(req.sqlQuery)
+    .then(r => {
       return res.send(r.rows);
     })
     .catch(function (err) {
-      console.log(err);
-      res.status(400).send('400 Bad request');
+      console.error(err);
+      res.status(500).send('500 Bad request');
     });
-};
+}
