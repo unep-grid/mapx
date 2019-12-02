ef5709f28343ac2b7085218ab061ca4f76bbf362
fxi <f@fxi.io>
2019-12-02 15:51:22 +0100

    Solved issue where 'view duration' was counted twice

--- app/src/js/mx_helper_log.js
+++ app/src/js/mx_helper_log.js
@@ -93,7 +93,7 @@ export function initLog() {
     idGroup: 'mx_log',
     callback: (d) => {
       if (h.isView(d.view)) {
-        d.view._added_at = Date.now();
+        d.view._log_added_at = Date.now();
         logger.add({
           id_log: 'view_add',
           data: {
@@ -111,8 +111,9 @@ export function initLog() {
     type: 'view_removed',
     idGroup: 'mx_log',
     callback: (d) => {
-      if (h.isView(d.view) && d.view._added_at > 0) {
-        let viewDuration = (Date.now() - d.view._added_at) / 1000;
+      if (h.isView(d.view) && d.view._log_added_at > 0) {
+        let viewDuration = (Date.now() - d.view._log_added_at) / 1000;
+        delete d.view._log_added_at;
         logger.add({
           id_log: 'view_remove',
           data: {
