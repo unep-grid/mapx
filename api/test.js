6a8b7960e114c900b1c7d0279ad3a89f119cfcef
drikc <cgampert@gmail.com>
2020-02-19 09:48:29 +0100

    ci-drone: fix mocha test exit

--- api/test.js
+++ api/test.js
@@ -3,4 +3,6 @@ var Mocha = require('mocha');
 var mocha = new Mocha();
 mocha.addFile('test/testMiscellaneous');
 mocha.addFile('test/testGetProjects');
-mocha.run();
+mocha.run(function() {
+  process.exit();
+});
