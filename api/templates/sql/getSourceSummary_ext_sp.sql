c9adf4650685f09ba05737c0b965b64a87ce1ff3
fxi <f@fxi.io>
2020-07-23 09:54:18 +0200

    Added WMS bbox query; refactor sliders; build layer summary bbox in sql; refactor WMS functions using async/await;

--- api/templates/sql/getSourceSummary_ext_sp.sql
+++ api/templates/sql/getSourceSummary_ext_sp.sql
@@ -1,4 +1,4 @@
-WITH layer_sp_ext as ( 
+WITH layer_geojson_ext as ( 
   SELECT 
   ST_AsGeoJSON(
     ST_Extent(
@@ -6,12 +6,21 @@ WITH layer_sp_ext as (
         ST_Envelope(geom)::geography
         , 1 )::geometry
     )
-  )
+  )::json #> '{"coordinates",0}'
   AS extent FROM "{{idSource}}"
+),
+layer_sp_ext as (
+  SELECT 
+  json_build_object(
+    'extent_sp', json_build_object(
+      'lat1', lse.extent #> '{3,1}',
+      'lng1', lse.extent #> '{3,0}',
+      'lat2', lse.extent #> '{1,1}',
+      'lng2', lse.extent #> '{1,0}'
+    )
+  )::json as extent
+  FROM layer_geojson_ext as lse
 )
 
-SELECT json_build_object(
-  'extent_sp', lse.extent::json
-) as res
-FROM layer_sp_ext as lse;
+SELECT extent::json as res from layer_sp_ext;
 
