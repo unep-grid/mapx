71e11cbf9b36903117ab735802fb6f8201cb8203
fxi <f@fxi.io>
2022-02-07 20:09:25 +0100

    Updated SDK deps;

--- app/src/js/sdk/dev/index.js
+++ app/src/js/sdk/dev/index.js
@@ -1,21 +1,31 @@
+const elMapx = document.getElementById('mapx');
+const elOut = document.getElementById('out');
+
 const mapx = new mxsdk.Manager({
   //url:   'http://dev.mapx.localhost:8880/static.html?project=MX-HPP-OWB-3SI-3FF-Q3R&views=MX-T8GJQ-GIC8X-AHLA9&zoomToViews=true&lat=-4.087&lng=21.754&z=4.886'
-  container: document.getElementById('mapx'),
-  url:
-    'http://dev.mapx.localhost:8880/?project=MX-HPP-OWB-3SI-3FF-Q3R&language=en'
-});
-
-mapx.on('message', (message) => {
-  if (message.level === 'log') {
-    console.info(`%c 🤓 ${message.text}`, 'color: #76bbf7');
-  } else if (message.level === 'message') {
-    console.info(`%c 😎 ${message.text}`, 'color: #70e497');
-  } else if (message.level === 'warning') {
-    console.info(`%c 🥴 ${message.text}`, 'color: #d09c23');
-  } else if (message.level === 'error') {
-    console.info(`%c 🤬 ${message.text}`, 'color: #F00');
+  container: elMapx,
+  url: {
+    host: 'dev.mapx.localhost',
+    port: 8880,
+    protocol: 'http'
+  },
+  static: true,
+  verbose: true,
+  params: {
+    zoomToViews: true,
+    closePanels: true,
+    //views: ['MX-CRPXN-W8IAC-XWFLU'],
+    views: ['MX-8C1KB-SE08V-JUMOH'],
+    language: 'en'
   }
 });
+
+mapx.on('ready', async () => {
+  await mapx.ask('set_features_click_sdk_only', {
+    enable: true
+  });
+});
+
 mapx.on('view_added', (o) => {
   console.log('view added', o);
 });
@@ -30,6 +40,6 @@ mapx.on('project_change', (d) => {
 });
 mapx.on('click_attributes', (d) => {
   console.log(`Attributes clicked part ${d.part}/${d.nPart}`);
+  const str = JSON.stringify(d, 0, 2);
+  out.innerText = str;
 });
-
-
