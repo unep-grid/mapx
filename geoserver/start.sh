8abbd27553539ee4ae9fceb887380dfa6a09844d
fxi <f@fxi.io>
2022-05-17 17:03:18 +0200

    Standardisation of docker users settings + data directories

--- geoserver/start.sh
+++ geoserver/start.sh
@@ -3,7 +3,7 @@
 #
 # Set default credential using custom variable
 #
-updateCred="<?xml version='1.0' encoding='UTF-8'?>\
+ADMIN_XML="<?xml version='1.0' encoding='UTF-8'?>\
   <userRegistry version='1.0' xmlns='http://www.geoserver.org/security/users'>\
   <users><user enabled='true' name='admin' password='plain:"${GEOSERVER_ADMIN_PASSWORD}"'/></users>\
   <groups/></userRegistry>" 
@@ -14,7 +14,7 @@ updateCred="<?xml version='1.0' encoding='UTF-8'?>\
 #
 if [ ! -d ${GEOSERVER_DATA_DIR}/security ]
 then
-  cp -R ${GEOSERVER_HOME}/data_dir ${GEOSERVER_DATA_DIR}
+  cp -R ${GEOSERVER_HOME}/data_dir/* ${GEOSERVER_DATA_DIR}/
 else
   echo "Data folder ${GEOSERVER_DATA_DIR} exists, don't copy default"
 fi
@@ -24,7 +24,9 @@ fi
 #
 echo "Update default password"
 
-echo ${updateCred} > ${GEOSERVER_DATA_DIR}/security/usergroup/default/users.xml
+echo ${ADMIN_XML} > ${GEOSERVER_DATA_DIR}/security/usergroup/default/users.xml
+
+export HTTPS_PROXY_NAME=$GEOSERVER_URL_PUBLIC
 
 sh ${GEOSERVER_HOME}"/bin/startup.sh"
 
