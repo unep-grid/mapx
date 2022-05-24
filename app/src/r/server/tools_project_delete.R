a5dd223900405e05e720e2e8427005b501a9650f
fxi <f@fxi.io>
2022-05-24 13:36:04 +0200

    Project deletion: removed remaining geoserver code

--- app/src/r/server/tools_project_delete.R
+++ app/src/r/server/tools_project_delete.R
@@ -1,66 +1,62 @@
 
-observeEvent(input$btnShowProjectDelete,{
-
+observeEvent(input$btnShowProjectDelete, {
   userRole <- getUserRole()
   idProject <- reactData$project
-  language <- reactData$language 
+  language <- reactData$language
   isAdmin <- isTRUE(userRole$admin)
   userData <- reactUser$data
-  isProjectDefault <- isTRUE(idProject == config[[c("project","default")]])
+  isProjectDefault <- isTRUE(idProject == config[[c("project", "default")]])
 
-  if( isAdmin && !isProjectDefault ){
-
+  if (isAdmin && !isProjectDefault) {
     projectData <- mxDbGetProjectData(idProject)
-    projectTitle <- .get(projectData,c("title",language))
-    if(noDataCheck(projectTitle)){
-      projectTitle <- .get(projectData,c("title","en"))
+    projectTitle <- .get(projectData, c("title", language))
+    if (noDataCheck(projectTitle)) {
+      projectTitle <- .get(projectData, c("title", "en"))
     }
     reactData$projectTitle <- projectTitle
 
     #
-    # Set public mode and 
+    # Set public mode and
     # create select input for members, publishers and admins
     #
     ui <- tagList(
       textInput(
         "txtProjectConfirmName",
-        label = d("project_delete_confirm_title",language,web=F),
-        ),
+        label = d("project_delete_confirm_title", language, web = F),
+      ),
       uiOutput("uiProjectDeleteValidation")
     )
 
     btnDelete <- actionButton(
       "btnDeleteProject",
-      d("btn_delete",language,web=F)
+      d("btn_delete", language, web = F)
     )
 
     mxModal(
       id = "deleteProject",
-      title = d("project_delete_title",language,web=F) + " \" " + projectTitle + " \" ",
+      title = d("project_delete_title", language, web = F) + " \" " + projectTitle + " \" ",
       content = ui,
-      textCloseButton = d("btn_cancel",language,web=F),
+      textCloseButton = d("btn_cancel", language, web = F),
       buttons = list(btnDelete)
     )
   }
-
 })
 
 
-observeEvent(input$txtProjectConfirmName,{
-
+observeEvent(input$txtProjectConfirmName, {
   language <- reactData$language
-  projectTitle <-  reactData$projectTitle
+  projectTitle <- reactData$projectTitle
   projectConfirmName <- input$txtProjectConfirmName
 
   errors <- logical(0)
   warning <- logical(0)
 
-  errors['error_title_no_match'] <- isTRUE( tolower(projectTitle) != tolower(projectConfirmName) )
+  errors["error_title_no_match"] <- isTRUE(tolower(projectTitle) != tolower(projectConfirmName))
   errors <- errors[errors]
   hasError <- length(errors) > 0
 
-  if(!hasError){
-    warning['warning_delete_project'] <- TRUE  
+  if (!hasError) {
+    warning["warning_delete_project"] <- TRUE
   }
 
 
@@ -79,137 +75,130 @@ observeEvent(input$txtProjectConfirmName,{
   )
 
   reactData$projectDeleteHasError <- hasError
-
 })
 
 
-observeEvent(input$btnDeleteProject,{
+observeEvent(input$btnDeleteProject, {
+  if (reactData$projectDeleteHasError) {
+    return()
+  }
 
-  if(reactData$projectDeleteHasError) return()
-
-  mxCatch(title="Delete project: search data to remove",{
-
+  mxCatch(title = "Delete project: search data to remove", {
     project <- reactData$project
-    projectTitle <-  reactData$projectTitle
+    projectTitle <- reactData$projectTitle
     language <- reactData$language
 
     mxModal(
       id = "deleteProject",
-      title = d("project_delete_title",language,web=F) + " \" " + projectTitle + " \" ",
-      content = d("project_delete_analyze",language),
+      title = d("project_delete_title", language, web = F) + " \" " + projectTitle + " \" ",
+      content = d("project_delete_analyze", language),
       buttons = list(),
       removeCloseButton = TRUE
     )
 
     sourcesToRemove <- mxDbGetQuery("
-      SELECT id 
+      SELECT id
       FROM mx_sources
-      WHERE project = '" + project + "'"
-      )$id
+      WHERE project = '" + project + "'")$id
     viewsProject <- mxDbGetQuery("
       SELECT distinct id
-      FROM mx_views_latest 
-      WHERE project = '" + project + "'"
-      )$id
+      FROM mx_views_latest
+      WHERE project = '" + project + "'")$id
 
     viewsToRemove <- c()
     #
     # Remove remaining views
     #
 
-    for( src in sourcesToRemove ){
-      viewsToRemove <-  c(mxDbGetViewsIdBySourceId(src)$id,viewsToRemove)
+    for (src in sourcesToRemove) {
+      viewsToRemove <- c(mxDbGetViewsIdBySourceId(src)$id, viewsToRemove)
     }
 
-    viewsToRemove <- c(viewsToRemove,viewsProject)
+    viewsToRemove <- c(viewsToRemove, viewsProject)
     viewsToRemove <- unique(viewsToRemove)
 
     reactData$projectDeleteViews <- viewsToRemove
     reactData$projectDeleteSources <- sourcesToRemove
-
   })
 
 
   ui <- tags$ul(
     tags$li(
-      tags$b(d("project_delete_number_of_sources",language))
-      ,':',
-      tags$span(length(sourcesToRemove))),
+      tags$b(d("project_delete_number_of_sources", language)),
+      ":",
+      tags$span(length(sourcesToRemove))
+    ),
     tags$li(
-      tags$b(d("project_delete_number_of_views",language))
-      ,':',
-      tags$span(length(viewsToRemove)))
+      tags$b(d("project_delete_number_of_views", language)),
+      ":",
+      tags$span(length(viewsToRemove))
+    )
   )
 
   btnDelete <- actionButton(
     "btnDeleteProjectConfirm",
-    d("btn_confirm",language,web=F)
+    d("btn_confirm", language, web = F)
   )
 
   mxModal(
     id = "deleteProject",
-    title = d("project_delete_title",language,web=F) + " \" " + projectTitle + " \" ",
+    title = d("project_delete_title", language, web = F) + " \" " + projectTitle + " \" ",
     content = ui,
-    textCloseButton = d("btn_cancel",language,web=F),
+    textCloseButton = d("btn_cancel", language, web = F),
     buttons = list(btnDelete)
   )
-
 })
 
 
-observeEvent(input$btnDeleteProjectConfirm,{
+observeEvent(input$btnDeleteProjectConfirm, {
+  mxCatch(title = "Project remove confirmed", {
+    if (reactData$projectDeleteHasError) {
+      return()
+    }
 
-  if(reactData$projectDeleteHasError) return()
+    views <- unique(reactData$projectDeleteViews)
+    sources <- unique(reactData$projectDeleteSources)
 
-  views <-  unique(reactData$projectDeleteViews)
-  sources <-  unique(reactData$projectDeleteSources)
+    userRole <- getUserRole()
+    idProject <- reactData$project
+    language <- reactData$language
+    isAdmin <- isTRUE(userRole$admin)
+    isProjectDefault <- isTRUE(idProject == config[[c("project", "default")]])
 
-  userRole <- getUserRole()
-  idProject <- reactData$project
-  language <- reactData$language 
-  isAdmin <- isTRUE(userRole$admin)
-  isProjectDefault <- isTRUE(idProject == config[[c("project","default")]])
+    if (!isAdmin || isProjectDefault) {
+      return()
+    }
 
-  if( !isAdmin || isProjectDefault ) return()
+    mxUpdateQueryParameters(list(
+      project = ""
+    ))
 
-  mxUpdateQueryParameters(list(
-      project = NULL
-      ))
+    #
+    # Remove all related views
+    #
+    for (v in views) {
+      mxDbGetQuery("delete from mx_views where id = '" + v + "'")
+    }
+    #
+    # Remove all related sources
+    #
+    for (s in sources) {
+      mxDbGetQuery("delete from mx_sources where id = '" + s + "'")
+      mxDbGetQuery("drop table if exists " + s)
+    }
+    #
+    # Remove project itself
+    #
+    mxDbGetQuery("delete from mx_projects where id = '" + idProject + "'")
 
-  #
-  # Remove all related views
-  #
-  for(v in views){
-    mxDbGetQuery("delete from mx_views where id = '" + v + "'")
-  }
-  #
-  # Remove all related sources
-  #
-  for(s in sources){
-    mxDbGetQuery("delete from mx_sources where id = '" + s + "'")
-    mxDbGetQuery("drop table if exists " + s)
-  }
-  #
-  # Remove project itself
-  #
-  mxDbGetQuery("delete from mx_projects where id = '" + idProject + "'")
+    #
+    # Force logout
+    #
+    mxModal(
+      id = "deleteProject",
+      close = TRUE
+    )
 
-  #
-  # Force logout
-  #
-  mxModal(
-    id='deleteProject',
-    close=TRUE
-  )
-  reactData$project <- .get(config,c('project','default'))
-  
-  #
-  # Remove Services
-  #
-  mxDeleteGeoServerAllProjectWorkspace(idProject)
-
-
-
+    reactData$project <- .get(config, c("project", "default"))
+  })
 })
-
-
