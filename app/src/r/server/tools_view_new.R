5f45b532590eed59217eff2fb1b3e814b5b15f86
fxi <f@fxi.io>
2022-06-02 16:02:39 +0200

    formating tools_view_new

--- app/src/r/server/tools_view_new.R
+++ app/src/r/server/tools_view_new.R
@@ -1,84 +1,80 @@
 
 
-observeEvent(input$btnAddView,{
+observeEvent(input$btnAddView, {
   isGuest <- isGuestUser()
   userRole <- getUserRole()
   isPublisher <- !isGuest && "publishers" %in% userRole$groups
 
-  if(!isPublisher){
+  if (!isPublisher) {
     return()
   }
 
   language <- reactData$language
 
   idView <- randomString(
-    prefix="MX-",
-    splitIn=3,
-    addLETTERS=T,
-    addLetters=F,
-    splitSep="-",
+    prefix = "MX-",
+    splitIn = 3,
+    addLETTERS = T,
+    addLetters = F,
+    splitSep = "-",
     sep = "-"
   )
 
 
-  typeChoices <- config[[c("views","type")]]
+  typeChoices <- config[[c("views", "type")]]
 
   names(typeChoices) <- d(unlist(typeChoices), language)
 
-  reactData$viewAddId<- idView
+  reactData$viewAddId <- idView
 
   buttons <- list(
     actionButton(
       inputId = "btnAddViewConfirm",
-      label = d("create",language),
-      disable=TRUE
+      label = d("create", language),
+      disable = TRUE
     )
   )
 
   mxModal(
     id = "createNewView",
-    title = d("view_new_text",language),
+    title = d("view_new_text", language),
     buttons = buttons,
-    textCloseButton = d("btn_cancel",language),
+    textCloseButton = d("btn_cancel", language),
     content = tags$div(
       selectizeInput(
         inputId = "selectViewType",
-        label = d("view_type_select",language),
+        label = d("view_type_select", language),
         choices = typeChoices,
-        options = list(
-        )
-        ),
+        options = list()
+      ),
       textInput(
         inputId = "txtViewTitle",
-        label = d("view_title",language),
+        label = d("view_title", language),
         value = idView
-        ),
+      ),
       uiOutput("uiViewTitleValidation")
     )
   )
-
-
 })
 
 
-observeEvent(input$txtViewTitle,{
-
-  title <- trimws(subPunct(input$txtViewTitle," "))
+observeEvent(input$txtViewTitle, {
+  title <- trimws(subPunct(input$txtViewTitle, " "))
   project <- reactData$project
   language <- reactData$language
   idView <- reactData$viewAddId
-  v <- .get(config,c('validation','input','nchar'))
+  v <- .get(config, c("validation", "input", "nchar"))
   errors <- logical(0)
   warning <- logical(0)
 
-  errors['error_title_short'] <- noDataCheck(title) || nchar(title) < v$viewTitle$min
-  errors['error_title_long'] <-  nchar(title) > v$viewTitle$max
-  errors['error_title_bad'] <- mxProfanityChecker(title)
-  errors['error_title_exists'] <-  mxDbViewTitleExists(
+  errors["error_title_short"] <- noDataCheck(title) || nchar(title) < v$viewTitle$min
+  errors["error_title_long"] <- nchar(title) > v$viewTitle$max
+  errors["error_title_bad"] <- mxProfanityChecker(title)
+  errors["error_title_exists"] <- mxDbViewTitleExists(
     title = title,
-    project= project,
+    project = project,
     languages = config[["languages"]][["list"]]
-    )
+  )
 
   errors <- errors[errors]
   hasError <- length(errors) > 0
@@ -87,33 +83,33 @@ observeEvent(input$txtViewTitle,{
     id = "btnAddViewConfirm",
     hide = FALSE,
     disable = hasError
-    )
+  )
 
-  output$uiViewTitleValidation<- renderUI(
+  output$uiViewTitleValidation <- renderUI(
     mxErrorsToUi(
       errors = errors,
       warning = warning,
       language = language
-      )
     )
+  )
 
   reactData$viewAddHasError <- hasError
   reactData$viewAddTitle <- title
-
 })
 
 
-observeEvent(input$btnAddViewConfirm,{
-
+observeEvent(input$btnAddViewConfirm, {
   project <- reactData$project
   userData <- reactUser$data
-  title  <- reactData$viewAddTitle
+  title <- reactData$viewAddTitle
   idView <- reactData$viewAddId
   language <- reactData$language
   viewType <- input$selectViewType
-  hasErrors  <- reactData$viewAddHasError
+  hasErrors <- reactData$viewAddHasError
 
-  if(hasErrors) return();
+  if (hasErrors) {
+    return()
+  }
 
   #
   # Role check
@@ -122,7 +118,7 @@ observeEvent(input$btnAddViewConfirm,{
   userRole <- getUserRole()
   isPublisher <- !isGuest && "publishers" %in% userRole$groups
 
-  if(!isPublisher){
+  if (!isPublisher) {
     return()
   }
 
@@ -130,17 +126,17 @@ observeEvent(input$btnAddViewConfirm,{
 
   #
   # view data skeleton
-  # 
+  #
   data <- list(
-    title =  list(),
+    title = list(),
     abstract = list()
-    )
+  )
 
   #
   # Set values data text
   #
-  data[[c("title", language )]] <- title
-  #data[[c("collections")]] <- as.list(collections)
+  data[[c("title", language)]] <- title
+  # data[[c("collections")]] <- as.list(collections)
 
   #
   # Row to add in db
@@ -150,23 +146,23 @@ observeEvent(input$btnAddViewConfirm,{
     project = project,
     editor = userData$id,
     date_modified = Sys.time(),
-    readers =  list(),
+    readers = list(),
     editors = list(),
     data = data,
     type = viewType
-    )
+  )
 
   mxDbAddRow(
-    data=newView,
-    table=.get(config,c("pg","tables","views"))
-    )
+    data = newView,
+    table = .get(config, c("pg", "tables", "views"))
+  )
 
   # edit flag
-  newView$`_edit` = TRUE 
+  newView$`_edit` <- TRUE
 
   # add this as new (empty) source
   mglUpdateViewsList(
-    id = .get(config,c("map","id")),
+    id = .get(config, c("map", "id")),
     viewsList = list(newView),
     render = TRUE,
     project = project
@@ -175,11 +171,8 @@ observeEvent(input$btnAddViewConfirm,{
   reactData$updateViewListFetchOnly <- runif(1)
 
   mxModal(
-    id="createNewView",
-    title=d("view_new_created",language),
-    content=tags$span(d("view_new_created",language))
-    )
-
+    id = "createNewView",
+    title = d("view_new_created", language),
+    content = tags$span(d("view_new_created", language))
+  )
 })
-
-
