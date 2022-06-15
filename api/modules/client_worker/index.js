e66b11e9c84a29135a7e44b8b4f92e751e9cdd88
fxi <f@fxi.io>
2022-06-15 14:54:54 +0200

    client_worker over socket : check for once method

--- api/modules/client_worker/index.js
+++ api/modules/client_worker/index.js
@@ -1,5 +1,5 @@
 import { randomString } from "#mapx/helpers";
-import { isArray } from "@fxi/mx_valid";
+import { isArray, isFunction } from "@fxi/mx_valid";
 
 export const mwClientWorker = (io) => {
   return (req, res, next) => {
@@ -11,10 +11,16 @@ export const mwClientWorker = (io) => {
      */
     res.clientJobRequest = async (id_resolver, data) => {
       const sockets = await io.in(idSocket).fetchSockets();
-      if (!isArray(sockets) || sockets.length === 0) {
+
+      const valid =
+        isArray(sockets) && sockets.length > 0 && isFunction(sockets[0].once);
+
+      if (!valid) {
         throw new Error(`No sockets for ${idSocket}`);
       }
+
       const socket = sockets[0];
+
       return new Promise((resolve, reject) => {
         const idJob = randomString("job_");
         /*
