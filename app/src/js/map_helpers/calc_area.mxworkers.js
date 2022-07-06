2e42e2f75b99a515884d7adbf8453333736fe85b
fxi <f@fxi.io>
2022-07-06 11:16:01 +0200

    formating calc_area worker

--- app/src/js/map_helpers/calc_area.mxworkers.js
+++ app/src/js/map_helpers/calc_area.mxworkers.js
@@ -1,22 +1,22 @@
-/* jshint esversion:6 */
-
-import {combine,buffer,bboxClip, area} from "@turf/turf";
-function sendMessage(m){postMessage({message:m});}
-function sendProgress(m){postMessage({progress:m});}
-function sendEnd(m){postMessage({end:m});}
+import { combine, buffer, bboxClip, area } from "@turf/turf";
+function sendMessage(m) {
+  postMessage({ message: m });
+}
+function sendEnd(m) {
+  postMessage({ end: m });
+}
 
 // Inital message
 postMessage({
   progress: 0,
-  message: "start"
+  message: "start",
 });
 
-
 // handle message send from the main thread
-onmessage = function(e) {
+onmessage = function (e) {
   var data = e.data;
   var out = 0;
-  var g =  data.geojson;
+  var g = data.geojson;
   var featuresLength = g.features.length;
   // get all abject in one
   sendMessage("Extracting geometry from " + featuresLength + " features ...");
@@ -25,14 +25,13 @@ onmessage = function(e) {
   g = combine(g);
 
   sendMessage("Calculating out ...");
-  g = buffer(g,0);
+  g = buffer(g, 0);
 
-  sendMessage("Clipping data to selected extent ..."); 
-  g = bboxClip(g.features[0],data.bbox);
+  sendMessage("Clipping data to selected extent ...");
+  g = bboxClip(g.features[0], data.bbox);
 
   sendMessage("Calculating area ...");
   out = area(g) * 1e-6;
 
   sendEnd(Math.round(out));
-
 };
