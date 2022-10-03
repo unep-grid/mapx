a8697446f06fbfc62b7ed51f50acb4edbea98834
fxi <f@fxi.io>
2022-10-03 16:21:34 +0200

    Fix issue whith role for sources to edit by role: wrong operator for editors selection

--- api/modules/template/sql/getSourcesListByRoles.sql
+++ api/modules/template/sql/getSourcesListByRoles.sql
@@ -20,8 +20,13 @@ SELECT
 FROM
   mx_sources
 WHERE
+  /* $1 idProject */
   project = $1
   AND (
+    /* $2 idUser */ 
     editor = $2
-    OR editors <@ $3::jsonb
+    OR editors ? $2::varchar
+    /* $3 user groups as stringified array e.g. '["admins","publishers"]' */
+    OR editors ?| ARRAY(SELECT jsonb_array_elements_text($3::jsonb)) 
   )
+ORDER BY title ASC 
