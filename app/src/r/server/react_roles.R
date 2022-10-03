4e26dfe9208cb2b3acf40b937045c49a12cc242a
fxi <f@fxi.io>
2022-10-03 16:22:20 +0200

    Formating

--- app/src/r/server/react_roles.R
+++ app/src/r/server/react_roles.R
@@ -2,37 +2,35 @@
 # Test if current logged user is guest
 #
 isGuestUser <- reactive({
-   a <-  .get(reactUser,c("data","email"))
-   b <-  .get(config,c("mail","guest"))   
-   if(noDataCheck(a)||noDataCheck(b)) stop("User can't be identified")
-   return(a == b)
+  a <- .get(reactUser, c("data", "email"))
+  b <- .get(config, c("mail", "guest"))
+
+  if (noDataCheck(a) || noDataCheck(b)) stop("User can't be identified")
+  return(identical(a, b))
 })
 
 #
 # Get user role
 #
 getUserRole <- reactive({
-
   mxDebugMsg("get user roles")
 
   roles <- list()
   idProject <- reactData$project
   idUser <- reactUser$data$id
-  
-  roles <- mxDbGetProjectUserRoles(idUser,idProject)
 
- return(roles)
+  roles <- mxDbGetProjectUserRoles(idUser, idProject)
 
+  return(roles)
 })
 
 #
-# Concatenate id user and project 
+# Concatenate id user and project
 # -> Can be used to trigger update of views list
-# exemple: 145@MX-QNN-G0C-I6F-GZB-MPA 
+# exemple: 145@MX-QNN-G0C-I6F-GZB-MPA
 #
 getUserAtProject <- reactive({
   idProject <- reactData$project
   idUser <- reactUser$data$id
-  sprintf('%s@%s',idUser,idProject)
+  sprintf("%s@%s", idUser, idProject)
 })
-
--- app/src/r/server/tools_project_main.R
+++ app/src/r/server/tools_project_main.R
@@ -2,90 +2,89 @@
 # Admin panel and button
 #
 observe({
-
-  usersAllowedToCreate <- .get(config,c("project","creation","usersAllowed"),default=list())
+  usersAllowedToCreate <- .get(config, c("project", "creation", "usersAllowed"), default = list())
   userRole <- getUserRole()
   userData <- reactUser$data
   isAdmin <- isTRUE(userRole$admin)
   isPublisher <- isTRUE(userRole$publisher)
-  isProjectCreator <-  isTRUE(userData$id %in% usersAllowedToCreate)
-  isProjectDefault <- isTRUE(reactData$project == .get(config,c("project","default")))
+  isProjectCreator <- isTRUE(userData$id %in% usersAllowedToCreate)
+  isProjectDefault <- isTRUE(reactData$project == .get(config, c("project", "default")))
   isGuest <- isGuestUser()
   reactData$projectAllowedToCreate <- isProjectCreator
 
-  if( isGuest ) return()
+  if (isGuest) {
+    return()
+  }
 
   isolate({
     language <- reactData$language
     toolsTitle <- tagList(
-      tags$h4(d("title_tools_admin_project",language,web=T))
-      )
-    
-    btns <- tagList();
+      tags$h4(d("title_tools_admin_project", language, web = T))
+    )
 
-    labelBtnAdd <- ifelse(isProjectCreator,"btn_show_add_project","btn_show_add_project_disabled")
-    labelBtnDelete <- ifelse(isProjectDefault,"btn_show_project_delete_disabled","btn_show_project_delete") 
+    btns <- tagList()
+
+    labelBtnAdd <- ifelse(isProjectCreator, "btn_show_add_project", "btn_show_add_project_disabled")
+    labelBtnDelete <- ifelse(isProjectDefault, "btn_show_project_delete_disabled", "btn_show_project_delete")
+
+
 
     btns <- tagList(
       btns,
       actionButton(
-        label = d(labelBtnAdd,language, web=T),
+        label = d(labelBtnAdd, language, web = T),
         inputId = "btnShowAddProject",
-        class = "btn btn-default " + ifelse(isProjectCreator,"","disabled")
-        )
+        class = "btn btn-default " + ifelse(isProjectCreator, "", "disabled")
       )
+    )
 
-    if ( isPublisher ){
+    if (isPublisher) {
       btns <- tagList(
         btns,
         actionButton(
-          label = d("btn_show_project_external_views",language,web=T),
+          label = d("btn_show_project_external_views", language, web = T),
           inputId = "btnShowProjectExternalViews",
           class = "btn btn-default"
-          )
         )
+      )
     }
 
-    if( isAdmin ){
-
+    if (isAdmin) {
       btns <- tagList(
         btns,
         actionButton(
-          label = d("btn_show_role_manager",language,web=T),
+          label = d("btn_show_role_manager", language, web = T),
           inputId = "btnShowRoleManager",
           class = "btn btn-default",
-          ),
+        ),
         actionButton(
-          label = d("btn_show_invite_member",language,web=T),
+          label = d("btn_show_invite_member", language, web = T),
           inputId = "btnShowInviteMember",
           class = "btn btn-default",
-          ),
+        ),
         actionButton(
-          label = d("btn_show_project_config",language,web=T),
+          label = d("btn_show_project_config", language, web = T),
           inputId = "btnShowProjectConfig",
           class = "btn btn-default",
-          ),
+        ),
         actionButton(
-          label =  d(labelBtnDelete,language,web=T),
+          label = d(labelBtnDelete, language, web = T),
           inputId = "btnShowProjectDelete",
-          class = "btn btn-default " + ifelse(isProjectDefault,"disabled",""),
-          ),
+          class = "btn btn-default " + ifelse(isProjectDefault, "disabled", ""),
+        ),
         actionButton(
-          label =  d('btn_show_project_views_states',language, web=T),
+          label = d("btn_show_project_views_states", language, web = T),
           inputId = "btnShowProjectViewsStates",
           class = "btn btn-default "
-          )
         )
+      )
     }
 
     editProject <- tagList(
       toolsTitle,
       btns
-      )
+    )
 
     output$uiBtnShowRoleManager <- renderUI(editProject)
-
   })
 })
-
-
