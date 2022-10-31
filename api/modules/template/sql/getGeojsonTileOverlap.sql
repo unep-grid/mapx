18b00a1a75d794eec468626a7e3b08f7bbe36f71
fxi <f@fxi.io>
2022-10-31 14:34:11 +0100

    Tiles overlap : fix logic issue prevented overlap to be created

--- api/modules/template/sql/getGeojsonTileOverlap.sql
+++ api/modules/template/sql/getGeojsonTileOverlap.sql
@@ -16,41 +16,38 @@ main as(
 ),
 overlap as (
   SELECT {{attributes_pg}},
-  CASE WHEN {{isPointGeom}} 
+  CASE WHEN {{isPointGeom}}
     THEN
+    /**  intersects => all points **/ 
+    m.{{geom}} 
+  ELSE
     CASE 
+      /** polygon entirely covered => all geom **/
       WHEN ST_CoveredBy(
         m.{{geom}},
         k.{{geom}}
       ) 
       THEN m.{{geom}} 
     ELSE
-      ST_Multi(
-        ST_Intersection(
-          k.{{geom}},
-          ST_MakeValid(m.{{geom}})
+      /** polygon partially covered => clipped **/
+        ST_Multi(
+          ST_Intersection(
+            k.{{geom}},
+            ST_MakeValid(m.{{geom}})
+          )
         )
-      )
-END
-ELSE
-  m.{{geom}} END as {{geom}}
-  FROM main m, mask k
-  WHERE ST_Intersects(m.geom,k.geom)
+    END 
+END as {{geom}}
+FROM main m, mask k
+WHERE ST_Intersects(m.geom,k.geom)
 ),
 simple as (
   SELECT {{attributes_pg}},
-  CASE 
-WHEN {{isPointGeom}}
-  -- no effect on single point 
-  THEN ST_RemoveRepeatedPoints(overlap.{{geom}})
-ELSE 
-  ST_simplify(overlap.{{geom}},(50/(512*(({{zoom}}+1)^2))))
-END geom
-FROM overlap
+  ST_simplify(overlap.{{geom}},(50/(512*(({{zoom}}+1)^2)))) geom
+  FROM overlap
 )
 
 
 SELECT {{attributes_pg}}, 
 ST_AsGeoJSON({{geom}}) geom 
 FROM simple
-
