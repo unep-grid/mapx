703d28a2934960cf42f2b3da0399f1164d076f1a
fxi <f@fxi.io>
2023-01-13 14:33:58 +0100

    mx_helpers : change module order

--- app/src/js/mx_helpers.js
+++ app/src/js/mx_helpers.js
@@ -1,66 +1,72 @@
-export * from './mx_helper_settings.js';
-export * from './mx_helper_cookies.js';
-export * from './mx_helper_log.js';
-export * from './mx_helper_utils_xml.js';
-export * from './mx_helper_utils_json.js';
-export * from './mx_helper_misc.js';
-export * from './mx_helper_modal.js';
-export * from './mx_helper_selectize.js';
-export * from './mx_helper_list_projects.js';
-export * from './mx_helper_intersect.js';
-export * from './mx_helper_upload_source.js';
-export * from './mx_helper_overlap_tool.js';
-export * from './mx_helper_validate_geom_tool.js';
-export * from './mx_helper_map_draw.js';
-export * from './mx_helper_ui_color.js';
-export * from './mx_helper_draggable.js';
-export * from './mx_helper_map_pixop.js';
-export * from './mx_helper_map_dragdrop.js';
-export * from './mx_helper_map_view_badges.js';
-export * from './mx_helper_map_view_build.js';
-export * from './mx_helper_map_view_fetch.js';
-export * from './mx_helper_map_project_fetch.js';
-export * from './mx_helper_map_view_click.js';
-export * from './mx_helper_map_view_style.js';
-export * from './mx_helper_map_view_ui.js';
-export * from './mx_helper_metadata_validation.js';
-export * from './mx_helper_map_view_metadata.js';
-export * from './mx_helper_canvas_star.js';
-export * from './mx_helper_canvas.js';
-export * from './mx_helper_jed.js';
-export * from './mx_helper_filter_views.js';
-export * from './mx_helper_source_summary.js';
-export * from './mx_helper_fetch_progress.js';
-export * from './mx_helper_root_mode.js';
-export * from './mx_helper_ip.js';
-export * from './mx_helper_sdk.js';
-export * from './mx_helper_issues.js';
+/*
+ *  Imported module available in mx.helpers:
+ *  ( access requested for widget code / cc code.)
+ *
+ */
+export * from "./map_composer/map_composer_instance.js";
+export * from "./source/display_table.js";
+export * from "./app_utils";
+export * from "./mirror_util";
+export * from "./language";
+export * from "./epsgio";
+export * from "./api_routes";
+export * from "./story_map";
+export * from "./modules_loader_async";
+export * from "./animation_frame";
+export * from "./array_stat";
+export * from "./color_utils";
+export * from "./el_mapx";
+export * from "./is_test/index.js";
+export * from "./is_test_mapx";
+export * from "./user_pref";
+export * from "./mapboxgl_json_editor";
+export * from "./commonloc";
+export * from "./load_test";
+export * from "./wms";
+export * from "./style_vt";
+export * from "./map_helpers";
+export * from "./legend_vt";
+export * from "./url_utils";
 
-/* 
-*  imported module available in mx.helpers:
-*  Access requested for widget code / cc code. 
-*/
-export * from './map_composer/map_composer_instance.js';
-export * from './source/display_table.js';
-export * from './app_utils';
-export * from './mirror_util';
-export * from './language';
-export * from './epsgio';
-export * from './api_routes';
-export * from './story_map';
-export * from './modules_loader_async';
-export * from './animation_frame';
-export * from './array_stat';
-export * from './color_utils';
-export * from './el_mapx';
-export * from './is_test/index.js';
-export * from './is_test_mapx';
-export * from './user_pref';
-export * from './mapboxgl_json_editor';
-export * from './commonloc';
-export * from './load_test';
-export * from './wms';
-export * from './style_vt';
-export * from './map_helpers';
-export * from './legend_vt';
-export * from './url_utils';
+/**
+ * Old modules that use global mx object
+ * TODO: convert as independent modules, as
+ * modules above
+ */
+export * from "./mx_helper_settings.js";
+export * from "./mx_helper_cookies.js";
+export * from "./mx_helper_log.js";
+export * from "./mx_helper_utils_xml.js";
+export * from "./mx_helper_utils_json.js";
+export * from "./mx_helper_misc.js";
+export * from "./mx_helper_modal.js";
+export * from "./mx_helper_selectize.js";
+export * from "./mx_helper_list_projects.js";
+export * from "./mx_helper_intersect.js";
+export * from "./mx_helper_upload_source.js";
+export * from "./mx_helper_overlap_tool.js";
+export * from "./mx_helper_validate_geom_tool.js";
+export * from "./mx_helper_map_draw.js";
+export * from "./mx_helper_ui_color.js";
+export * from "./mx_helper_draggable.js";
+export * from "./mx_helper_map_pixop.js";
+export * from "./mx_helper_map_dragdrop.js";
+export * from "./mx_helper_map_view_badges.js";
+export * from "./mx_helper_map_view_build.js";
+export * from "./mx_helper_map_view_fetch.js";
+export * from "./mx_helper_map_project_fetch.js";
+export * from "./mx_helper_map_view_click.js";
+export * from "./mx_helper_map_view_style.js";
+export * from "./mx_helper_map_view_ui.js";
+export * from "./mx_helper_metadata_validation.js";
+export * from "./mx_helper_map_view_metadata.js";
+export * from "./mx_helper_canvas_star.js";
+export * from "./mx_helper_canvas.js";
+export * from "./mx_helper_jed.js";
+export * from "./mx_helper_filter_views.js";
+export * from "./mx_helper_source_summary.js";
+export * from "./mx_helper_fetch_progress.js";
+export * from "./mx_helper_root_mode.js";
+export * from "./mx_helper_ip.js";
+export * from "./mx_helper_sdk.js";
+export * from "./mx_helper_issues.js";
