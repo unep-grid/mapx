77b47087aaead5b48e0594f900fa4d6818cdd640
fxi <f@fxi.io>
2023-01-18 14:00:33 +0100

    SLD creation. Solved issue with paint expression, view with sld already as style._sld, and bug with the test for view with custom style

--- app/src/js/style_vt/view_to_mbstyle.js
+++ app/src/js/style_vt/view_to_mbstyle.js
@@ -2,7 +2,7 @@ import { colorToHex } from "./../color_utils";
 import { getView, getMap, getViewTitle } from "../map_helpers/index.js";
 import { getViewMapboxLayers } from "./view_to_mb_layers";
 import { mapboxToSld } from "./mbstyle_to_sld";
-import { isViewVtWithStyleCustom } from "./../is_test/index.js";
+import { isViewVtWithStyleCustom, isString } from "./../is_test/index.js";
 
 /**
  * Create sld style from view's layers
@@ -41,6 +41,11 @@ export async function getViewMapboxStyle(idView, opt) {
     simplifyExpression = false,
   } = opt || {};
 
+  /**
+   * TODO: set simplifyColors as an option
+   */
+  const simplifyColors = simplifyExpression;
+
   const view = getView(idView);
   const base = await getViewMapboxLayers(view, {
     useLabelAsId,
@@ -63,9 +68,16 @@ export async function getViewMapboxStyle(idView, opt) {
   for (const layer of base.layers) {
     if (layer.paint) {
       for (const k in layer.paint) {
-        /* rgb / rgba not yet supported by sld */
-        if (/color$/.test(k)) {
-          layer.paint[k] = colorToHex(layer.paint[k]);
+        /*
+         * rgb / rgba not yet supported by sld
+         */
+        if (simplifyColors && /color$/.test(k)) {
+          const color = layer.paint[k];
+          if (isString(color)) {
+            layer.paint[k] = colorToHex(color);
+          } else {
+            console.warn("Non-string color found: skipping conversion ", color);
+          }
         }
 
         /* outline do not render well in sld, better removing it */
