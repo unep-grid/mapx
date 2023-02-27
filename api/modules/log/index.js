5748fb780c6f873882fa6222c850887855b7fbee
fxi <f@fxi.io>
2023-02-27 15:25:40 +0100

    API: trying to solve 'stream is not readable' in 'get ip' middleware

--- api/modules/log/index.js
+++ api/modules/log/index.js
@@ -1,44 +1,44 @@
-import {Sql} from 'sql-ts';
-import bodyParser from 'body-parser';
-import Ajv from 'ajv';
-import {pgWrite} from '#mapx/db';
-import {mwSet} from '#mapx/ip';
-import {apiLogs} from '#mapx/schema';
+import { Sql } from "sql-ts";
+import bodyParser from "body-parser";
+import Ajv from "ajv";
+import { pgWrite } from "#mapx/db";
+import { mwSet } from "#mapx/ip";
+import { apiLogs } from "#mapx/schema";
 
-const sql = new Sql('postgres');
+const sql = new Sql("postgres");
 const v = new Ajv();
 const check = v.compile(apiLogs);
 /**
  * log table descriptiom
  */
 const tblLogs = sql.define({
-  name: 'mx_logs',
+  name: "mx_logs",
   columns: [
-    'date_modified',
-    'side',
-    'level',
-    'id_log',
-    'ip_user',
-    'id_user',
-    'id_project',
-    'is_guest',
-    'is_static',
-    'data'
-  ]
+    "date_modified",
+    "side",
+    "level",
+    "id_log",
+    "ip_user",
+    "id_user",
+    "id_project",
+    "is_guest",
+    "is_static",
+    "data",
+  ],
 });
 
 /**
  * Middleware stack
  */
 export const mwCollect = [mwSet, bodyParser.json(), mwCollectHelper];
-export default {mwCollect};
+export default { mwCollect };
 
 /**
  * Collect
  */
 async function mwCollectHelper(req, res) {
   try {
-    const {ipGeo} = req;
+    const ipGeo = req?._ip_geo || {};
     const isValid = validateLogs(req.body.logs);
     if (isValid) {
       await saveLogs(req.body.logs, ipGeo);
@@ -75,13 +75,12 @@ function formatLogs(logs, ipGeo) {
   for (const l of logs) {
     l.ip_user = ipGeo.ip;
     l.date_modified = new Date(l.date_modified);
-    if (l.id_log === 'session_start') {
+    if (l.id_log === "session_start") {
       l.data = {
         ...ipGeo,
-        ...l.data
+        ...l.data,
       };
     }
   }
   return logs;
 }
-
