314051e3adfd46f5e0cab6cc13a3843d1df3fe7c
fxi <f@fxi.io>
2023-03-02 12:07:17 +0100

    Fixes #893

--- app/src/js/source/edit_table.js
+++ app/src/js/source/edit_table.js
@@ -1056,6 +1056,9 @@ export class EditTableSessionClient extends WsToolsBase {
       allowInsertRow: false,
       renderAllRows: false,
       maxRows: table.data.length,
+      copyPaste: {
+        rowsLimit: table.data.length,
+      },
       dropdownMenu: [
         "filter_by_condition",
         "filter_operators",
