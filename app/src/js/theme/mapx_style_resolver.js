82673e7abbbd95842030c1e07bf2962bf8967eeb
fxi <f@fxi.io>
2023-03-20 16:45:07 +0100

    Improved bathymetry layers + style

--- app/src/js/theme/mapx_style_resolver.js
+++ app/src/js/theme/mapx_style_resolver.js
@@ -1,4 +1,4 @@
-import chroma from 'chroma-js';
+import chroma from "chroma-js";
 
 export function css_resolver(c) {
   return `
@@ -21,272 +21,290 @@ export function css_resolver(c) {
 }
 
 function v(col) {
-  const hide = !col.visibility || col.visibility !== 'visible';
-  const isStringCol = typeof col === 'string';
+  const hide = !col.visibility || col.visibility !== "visible";
+  const isStringCol = typeof col === "string";
   const color = isStringCol ? col : col.color;
   const chromaColor = chroma(color);
   if (hide) {
     return chromaColor.alpha(0).css();
   }
-  const alpha = (isStringCol || !col.alpha ) ? chromaColor.alpha() : col.alpha;
+  const alpha = isStringCol || !col.alpha ? chromaColor.alpha() : col.alpha;
   return chromaColor.alpha(alpha).css();
 }
 
 export function layer_resolver(c) {
   return [
     {
-      id: ['background'],
+      id: ["background"],
       layout: {
-        visibility: c.mx_map_background.visibility
+        visibility: c.mx_map_background.visibility,
       },
       paint: {
-        'background-color': c.mx_map_background.color
-      }
+        "background-color": c.mx_map_background.color,
+      },
     },
     {
-      id: ['hillshading'],
+      id: ["hillshading"],
       layout: {
         visibility: allVisible([
           c.mx_map_hillshade_shadow.visibility,
-          c.mx_map_hillshade_highlight.visibility
-        ])
+          c.mx_map_hillshade_highlight.visibility,
+        ]),
       },
       paint: {
-        'fill-color': [
-          'match',
-          ['get', 'class'],
-          'highlight',
+        "fill-color": [
+          "match",
+          ["get", "class"],
+          "highlight",
           c.mx_map_hillshade_highlight.color,
-          'shadow',
+          "shadow",
           c.mx_map_hillshade_shadow.color,
-          'hsla(0, 0%, 0%, 0)'
-        ]
-      }
+          "hsla(0, 0%, 0%, 0)",
+        ],
+      },
     },
     {
-      id: ['water'],
+      id: ["water"],
       layout: {
-        visibility: c.mx_map_water.visibility
+        visibility: c.mx_map_water.visibility,
       },
       paint: {
-        'fill-color': c.mx_map_water.color
-      }
+        "fill-color": c.mx_map_water.color,
+      },
     },
     {
-      id: ['bathymetry'],
+      id: ["bathymetry"],
       layout: {
         visibility: allVisible([
           c.mx_map_bathymetry_low.visibility,
-          c.mx_map_bathymetry_high.visibility
-        ])
+          c.mx_map_bathymetry_high.visibility,
+        ]),
       },
       paint: {
-        'fill-color': [
-          'interpolate',
-          ['cubic-bezier', 0, 0.5, 1, 0.5],
-          ['get', 'depth'],
+        "fill-color": [
+          "interpolate",
+          ["cubic-bezier", 0, 0.5, 1, 0.5],
+          ["get", "depth"],
           200,
           c.mx_map_bathymetry_high.color,
           9000,
-          c.mx_map_bathymetry_low.color
-        ]
-      }
+          c.mx_map_bathymetry_low.color,
+        ],
+      },
     },
     {
-      id: ['waterway'],
+      id: ["bathymetry-lines"],
       layout: {
-        visibility: c.mx_map_water.visibility
+        visibility: c.mx_map_bathymetry_lines.visibility,
       },
       paint: {
-        'line-color': c.mx_map_water.color
-      }
+        "line-color": c.mx_map_bathymetry_lines.color,
+      },
     },
     {
-      id: ['country-code'],
+      id: ["bathymetry-labels"],
       layout: {
-        visibility: c.mx_map_mask.visibility
+        visibility: allVisible([
+          c.mx_map_bathymetry_labels.visibility,
+          c.mx_map_bathymetry_labels_outline.visibility,
+        ]),
       },
       paint: {
-        'fill-color': c.mx_map_mask.color
-      }
+        "text-color": c.mx_map_bathymetry_labels.color,
+        "text-halo-color": c.mx_map_bathymetry_labels_outline.color,
+      },
+    },
+    {
+      id: ["waterway"],
+      layout: {
+        visibility: c.mx_map_water.visibility,
+      },
+      paint: {
+        "line-color": c.mx_map_water.color,
+      },
+    },
+    {
+      id: ["country-code"],
+      layout: {
+        visibility: c.mx_map_mask.visibility,
+      },
+      paint: {
+        "fill-color": c.mx_map_mask.color,
+      },
     },
     {
       id: [
-        'road-street-low',
-        'road-street_limited-low',
-        'road-path',
-        'road-construction',
-        'road-trunk_link',
-        'road-motorway_link',
-        'road-service-link-track',
-        'road-street_limited',
-        'road-street',
-        'road-secondary-tertiary',
-        'road-primary',
-        'road-trunk',
-        'road-motorway',
-        'road-rail',
-        'road-rail-tracks'
+        "road-street-low",
+        "road-street_limited-low",
+        "road-path",
+        "road-construction",
+        "road-trunk_link",
+        "road-motorway_link",
+        "road-service-link-track",
+        "road-street_limited",
+        "road-street",
+        "road-secondary-tertiary",
+        "road-primary",
+        "road-trunk",
+        "road-motorway",
+        "road-rail",
+        "road-rail-tracks",
       ],
       layout: {
-        visibility: c.mx_map_road.visibility
+        visibility: c.mx_map_road.visibility,
       },
       paint: {
-        'line-color': c.mx_map_road.color
-      }
+        "line-color": c.mx_map_road.color,
+      },
     },
     {
-      id: ['road-pedestrian-polygon', 'road-polygon'],
+      id: ["road-pedestrian-polygon", "road-polygon"],
       layout: {
-        visibility: c.mx_map_road.visibility
+        visibility: c.mx_map_road.visibility,
       },
       paint: {
-        'fill-color': c.mx_map_road.color
-      }
+        "fill-color": c.mx_map_road.color,
+      },
     },
     {
       id: [
-        'road-pedestrian-polygon-case',
-        'road-service-link-track-case',
-        'road-street_limited-case',
-        'road-street-case',
-        'road-secondary-tertiary-case',
-        'road-primary-case',
-        'road-motorway_link-case',
-        'road-trunk_link-case',
-        'road-trunk-case',
-        'road-motorway-case'
+        "road-pedestrian-polygon-case",
+        "road-service-link-track-case",
+        "road-street_limited-case",
+        "road-street-case",
+        "road-secondary-tertiary-case",
+        "road-primary-case",
+        "road-motorway_link-case",
+        "road-trunk_link-case",
+        "road-trunk-case",
+        "road-motorway-case",
       ],
       layout: {
-        visibility: c.mx_map_road_border.visibility
+        visibility: c.mx_map_road_border.visibility,
       },
       paint: {
-        'line-color': c.mx_map_road_border.color
-      }
+        "line-color": c.mx_map_road_border.color,
+      },
     },
     {
-      id: ['building'],
+      id: ["building"],
       layout: {
-        visibility: c.mx_map_building.visibility
+        visibility: c.mx_map_building.visibility,
       },
       paint: {
-        'fill-color': c.mx_map_building.color
-      }
+        "fill-color": c.mx_map_building.color,
+      },
     },
     {
-      id: ['boundary_un_1'],
+      id: ["boundary_un_1"],
       layout: {
-        visibility: c.mx_map_boundary_un_1.visibility
+        visibility: c.mx_map_boundary_un_1.visibility,
       },
       paint: {
-        'line-color': c.mx_map_boundary_un_1.color
-      }
+        "line-color": c.mx_map_boundary_un_1.color,
+      },
     },
     {
-      id: ['boundary_un_2'],
+      id: ["boundary_un_2"],
       layout: {
-        visibility: c.mx_map_boundary_un_1.visibility
+        visibility: c.mx_map_boundary_un_1.visibility,
       },
       paint: {
-        'line-color': c.mx_map_boundary_un_2.color
-      }
+        "line-color": c.mx_map_boundary_un_2.color,
+      },
     },
     {
-      id: ['boundary_un_3'],
+      id: ["boundary_un_3"],
       layout: {
-        visibility: c.mx_map_boundary_un_3.visibility
+        visibility: c.mx_map_boundary_un_3.visibility,
       },
       paint: {
-        'line-color': c.mx_map_boundary_un_3.color
-      }
+        "line-color": c.mx_map_boundary_un_3.color,
+      },
     },
     {
-      id: ['boundary_un_4'],
+      id: ["boundary_un_4"],
       layout: {
-        visibility: c.mx_map_boundary_un_4.visibility
+        visibility: c.mx_map_boundary_un_4.visibility,
       },
       paint: {
-        'line-color': c.mx_map_boundary_un_4.color
-      }
+        "line-color": c.mx_map_boundary_un_4.color,
+      },
     },
     {
-      id: ['boundary_un_8'],
+      id: ["boundary_un_8"],
       layout: {
-        visibility: c.mx_map_boundary_un_8.visibility
+        visibility: c.mx_map_boundary_un_8.visibility,
       },
       paint: {
-        'line-color': c.mx_map_boundary_un_8.color
-      }
+        "line-color": c.mx_map_boundary_un_8.color,
+      },
     },
     {
-      id: ['boundary_un_9'],
+      id: ["boundary_un_9"],
       layout: {
-        visibility: c.mx_map_boundary_un_9.visibility
+        visibility: c.mx_map_boundary_un_9.visibility,
       },
       paint: {
-        'line-color': c.mx_map_boundary_un_9.color
-      }
+        "line-color": c.mx_map_boundary_un_9.color,
+      },
     },
     {
-      id: ['boundary_un_6'],
+      id: ["boundary_un_6"],
       layout: {
-        visibility: c.mx_map_boundary_un_6.visibility
+        visibility: c.mx_map_boundary_un_6.visibility,
       },
       paint: {
-        'line-color': c.mx_map_boundary_un_6.color
-      }
+        "line-color": c.mx_map_boundary_un_6.color,
+      },
     },
     {
-      id: [
-        'place-label-capital',
-        'place-label-city',
-        'country-label'
-      ],
+      id: ["place-label-capital", "place-label-city", "country-label"],
       layout: {
         visibility: allVisible([
           c.mx_map_text_place.visibility,
-          c.mx_map_text_place_outline.visibility
-        ])
+          c.mx_map_text_place_outline.visibility,
+        ]),
       },
       paint: {
-        'text-color': c.mx_map_text_place.color,
-        'text-halo-color': c.mx_map_text_place_outline.color,
-        'icon-color': c.mx_map_text_place.color
-      }
+        "text-color": c.mx_map_text_place.color,
+        "text-halo-color": c.mx_map_text_place_outline.color,
+        "icon-color": c.mx_map_text_place.color,
+      },
     },
     {
-      id: ['water-label-point', 'water-label-line'],
+      id: ["water-label-point", "water-label-line"],
       layout: {
         visibility: allVisible([
           c.mx_map_text_water.visibility,
-          c.mx_map_text_water_outline.visibility
-        ])
+          c.mx_map_text_water_outline.visibility,
+        ]),
       },
       paint: {
-        'text-color': c.mx_map_text_water.color,
-        'text-halo-color': c.mx_map_text_water_outline.color,
-        'icon-color': c.mx_map_text_water.color
-      }
+        "text-color": c.mx_map_text_water.color,
+        "text-halo-color": c.mx_map_text_water_outline.color,
+        "icon-color": c.mx_map_text_water.color,
+      },
     },
     {
-      id: ['road-label'],
+      id: ["road-label"],
       layout: {
         visibility: allVisible([
           c.mx_map_text_road.visibility,
-          c.mx_map_text_road_outline.visibility
-        ])
+          c.mx_map_text_road_outline.visibility,
+        ]),
       },
       paint: {
-        'text-color': c.mx_map_text_road.color,
-        'text-halo-color': c.mx_map_text_road_outline.color
-      }
-    }
+        "text-color": c.mx_map_text_road.color,
+        "text-halo-color": c.mx_map_text_road_outline.color,
+      },
+    },
   ];
 }
 
 function allVisible(arr) {
   const v = arr.reduce((a, c) => {
-    return a === true && c === 'visible';
+    return a === true && c === "visible";
   }, true);
-  return v ? 'visible' : 'none';
+  return v ? "visible" : "none";
 }
