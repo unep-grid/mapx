54286ffd12257b3fb8c4e176f880b032f780f8f9
fxi <f@fxi.io>
2023-07-11 15:37:12 +0200

    health : set rate limit to 500 / 15 min

--- api/modules/health/index.js
+++ api/modules/health/index.js
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 500, // limit each IP to 500 requests per windowMs
   message: "Too many requests, please try again later.",
 });
 
