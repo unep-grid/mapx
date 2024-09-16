bbb7946390cf5bf9e07e1c3f1875aa3af09b2ec2
fxi <f@fxi.io>
2024-09-16 16:00:43 +0200

    OGC service template: should solve issue with <pre> layout change after a resize + wrong bbox order for wms 1.3.0 + srs->crs parameter name fixed

--- app/src/js/geoserver/index.js
+++ app/src/js/geoserver/index.js
@@ -50,18 +50,19 @@ export async function getGeoserverLayerPreviewFromView(idView, format = "png") {
   const meta = await getViewMetadata(idView);
   const bbox = await getViewExtent(idView);
   const geoserverUrl = await getGeoserverUrl();
-  // -180,-90,180,90
-  const bboxStr = `${bbox.lng1},${bbox.lat1},${bbox.lng2},${bbox.lat2}`;
+
+  // For WMS 1.3.0 with CRS=EPSG:4326, BBOX is minLat,minLon,maxLat,maxLon
+  const bboxStr = `${bbox.lat1},${bbox.lng1},${bbox.lat2},${bbox.lng2}`;
 
   const params = new URLSearchParams({
     service: "WMS",
-    version: "1.1.0",
+    version: "1.3.0",
     request: "GetMap",
     layers: `${meta.project}:${meta.id}`,
     bbox: bboxStr,
     width: "768",
     height: "766",
-    srs: "EPSG:4326",
+    crs: "EPSG:4326",
     styles: "",
     format: format === "png" ? "image/png" : "application/openlayers2",
   });
