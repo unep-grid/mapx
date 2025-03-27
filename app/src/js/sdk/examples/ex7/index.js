64b89f3c5f7c7d9b013b6051725224f3cff62f41
fxi <f@fxi.io>
2025-03-27 12:05:21 +0100

    style(sdk-examples): formating

--- app/src/js/sdk/examples/ex7/index.js
+++ app/src/js/sdk/examples/ex7/index.js
@@ -2,17 +2,20 @@ function makeApp(mcId, viewId) {
   const mc = document.getElementById(mcId);
   const m = new mxsdk.Manager({
     url: {
-      host: 'dev.mapx.localhost',
-      port: 8880
+      host: "dev.mapx.localhost",
+      port: 8880,
     },
     container: mc,
     static: true,
-    verbose: true
+    verbose: true,
+    params: {
+      closePanels: true,
+    },
   });
   window[mcId] = m;
-  m.on('ready', async () => {
-    mc.classList.add('loaded');
-    await m.ask('set_immersive_mode', {toggle: true});
+  m.on("ready", async () => {
+    mc.classList.add("loaded");
+    await m.ask("set_immersive_mode", { toggle: true });
     await m.ask("view_add", { idView: viewId });
     const view = await m.ask("get_view_meta", { idView: viewId });
     const info = document.createElement("div");
@@ -22,7 +25,6 @@ function makeApp(mcId, viewId) {
   });
 }
 
-
-makeApp('mapx1', 'MX-6EH7N-INM1L-P1PJ9');
-makeApp('mapx2', 'MX-ML9PZ-PZ1SI-WVV85');
-makeApp('mapx3', 'MX-3PH1G-OOJHT-6FS0X');
+makeApp("mapx1", "MX-6EH7N-INM1L-P1PJ9");
+makeApp("mapx2", "MX-ML9PZ-PZ1SI-WVV85");
+makeApp("mapx3", "MX-3PH1G-OOJHT-6FS0X");
